ca1569c8e3c74a694549b5b10e7e028d23b17cf8
d12frosted <d12frosted@gmail.com>
2015-03-20 22:51:26 +0200

    remove coloring functions

--- fish/config.fish
+++ fish/config.fish
@@ -4,18 +4,6 @@ set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/
 set -x EDITOR "emacs"
 set fish_greeting
 
-# coloring
-
-set -g black (set_color black)
-set -g red (set_color red)
-set -g green (set_color green)
-set -g yellow (set_color yellow)
-set -g blue (set_color blue)
-set -g purple (set_color purple)
-set -g cyan (set_color cyan)
-set -g white (set_color white)
-set -g normal (set_color normal)
-
 # git
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)<%an> -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
