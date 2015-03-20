c3d4614cb2a2378b11ef877029fc9b1834ef0cb8
d12frosted <d12frosted@gmail.com>
2015-03-20 22:50:37 +0200

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,67 +1,38 @@
+function fish_prompt --description 'd12frosted prompt'
+	set -l last_status $status
+
+  __prompt_duration
+
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  __terlar_git_prompt
+  __fish_hg_prompt
+  # echo
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  else
+    set_color green
+  end
+
+  echo -n ' λ '
+  set_color normal
+end
+
 function __prompt_duration
   if test $CMD_DURATION
     if test $CMD_DURATION -ge 8000
-      echo -es $white '~> duration: ' $yellow $CMD_DURATION ' ms' $normal
+      set_color white
+      echo -esn '  ~> duration: '
+      set_color yellow
+      echo -es $CMD_DURATION ' ms'
+      set_color normal
       if hash terminal-notifier ^ /dev/null
         echo -es 'Finished in ' $CMD_DURATION ' ms' | terminal-notifier
       end
     end
   end
-end
-
-function __prompt_dir
-  echo -ens $blue (pwd | sed 's:^$HOME:~:') $normal
-end
-
-function __prompt_git
-  set -l git_branch (_git_branch_name)
-  if test $git_branch
-    set -l is_dirty (_git_is_dirty)
-    set -l is_cherry (_git_is_cherry)
-
-    echo -ens ' · ('
-
-    # if dirty or contains commits that are not
-    # merged to upstream print helper text in red
-    if test $is_dirty -o $is_cherry
-      echo -ens $red
-    else
-      echo -ens $green
-    end
-
-    echo -ens "$git_branch"
-
-    # * - for dirty repository
-    if test $is_dirty
-      echo -ens ' *'
-    end
-
-    # @ - for repository with unmerged commits
-    if test $is_cherry
-      echo -ens ' @'
-    end
-
-    echo -ens $normal ')'
-
-  end
-end
-
-function __prompt_lambda
-  if test $RETVAL -ne 0
-    echo -ens $red
-  else
-    echo -ens $green
-  end
-  echo -ens 'λ ' $normal
-end
-
-function fish_prompt
-  set -g RETVAL $status
-
-  __prompt_duration
-  echo -e ''
-  __prompt_dir
-  __prompt_git
-  echo -e ''
-  __prompt_lambda
 end
