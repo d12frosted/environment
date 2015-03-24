9e2ccd3efeaf5ae6d1f0977e2f0e15b97c976d6f
d12frosted <d12frosted@gmail.com>
2015-03-24 09:26:44 +0200

    remove unused functions

--- fish/config.fish
+++ fish/config.fish
@@ -10,23 +10,6 @@ git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset %C(bol
 
 # functions
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
-function _git_is_cherry
-  set -l cherry_count (count (command git cherry -v ^ /dev/null))
-  if test $cherry_count -ne 0
-    echo $cherry_count
-  else
-    echo ''
-  end
-end
-
 function e
   emacs $argv
 end
