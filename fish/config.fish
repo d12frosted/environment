758e5f5898ddd37bd337078d883551764b0c73c0
d12frosted <d12frosted@icloud.com>
2015-04-17 11:21:01 +0300

    use emacsclient as editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # variables
 
 set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/.bin $HOME/.rvm/bin
-set -x EDITOR "emacs"
+set -x EDITOR "emacsclient"
 set fish_greeting
 
 # git
