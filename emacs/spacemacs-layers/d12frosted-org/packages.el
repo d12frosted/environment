189210529b279d5ccedb54bf507004d7a472c042
d12frosted <d12frosted@gmail.com>
2015-04-19 16:40:27 +0300

    fix org mode configurations location
    accidentally I put them in extensions.el instead of packages.el

--- emacs/spacemacs-layers/d12frosted-org/packages.el
+++ emacs/spacemacs-layers/d12frosted-org/packages.el
@@ -12,12 +12,61 @@
 (defvar d12frosted-org-packages
   '(
     ;; package d12frosted-orgs go here
+    org
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
 (defvar d12frosted-org-excluded-packages '()
   "List of packages to exclude.")
+
+(defun d12frosted-org/init-org ()
+  "Initialize org package."
+  (use-package org
+    :defer t
+
+    :init
+
+    :config
+    (defvar d12frosted/org-home-path "~/Dropbox/org")
+
+    (defvar d12frosted/org-agenda-ignore-dirs
+      (-map (lambda (dir) (d12frosted/concat-path d12frosted/org-home-path dir))
+            '("temporary"
+              "tmp")))
+
+    (defvar d12frosted/org-agenda-dirs
+      (d12frosted/directory-dirs d12frosted/org-home-path))
+
+    (defvar d12frosted/org-agenda-files
+      (-flatten (-map (lambda (dir) (d12frosted/org-files-in-folder dir)) d12frosted/org-agenda-dirs)))
+
+    (defadvice org-mode-flyspell-verify (after org-mode-flyspell-verify-hack activate)
+      (let ((rlt ad-return-value)
+            (begin-regexp "^[ \t]*#\\+begin_\\(src\\|html\\|latex\\)")
+            (end-regexp "^[ \t]*#\\+end_\\(src\\|html\\|latex\\)")
+            old-flag
+            b e)
+        (when ad-return-value
+          (save-excursion
+            (setq old-flag case-fold-search)
+            (setq case-fold-search t)
+            (setq b (re-search-backward begin-regexp nil t))
+            (if b (setq e (re-search-forward end-regexp nil t)))
+            (setq case-fold-search old-flag))
+          (if (and b e (< (point) e)) (setq rlt nil)))
+        (setq ad-return-value rlt)))
+
+    (setq org-todo-keywords '((sequence "BLOCKED" "TODO" "DELAYED" "STARTED" "TEST" "|" "DONE" "PASS"))
+          org-src-fontify-natively t
+          org-directory d12frosted/org-home-path
+          org-agenda-files d12frosted/org-agenda-files
+          org-agenda-inhibit-startup nil)
+
+    (define-key org-mode-map (kbd "C-c s o") 'd12frosted/org-sort-current-level)
+    (define-key org-mode-map (kbd "C-c s O") 'd12frosted/org-sort-upper-level)
+
+    (add-hook 'org-mode-hook 'd12frosted/org-auto-insert-template)))
 
 ;; For each package, define a function d12frosted-org/init-<package-d12frosted-org>
 ;;
