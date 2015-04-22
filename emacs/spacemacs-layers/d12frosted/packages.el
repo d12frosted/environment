4dfaacbcc5da8718892de57c7d0eae88a89405ab
d12frosted <d12frosted@icloud.com>
2015-04-22 14:23:10 +0300

    add elfeed package

--- emacs/spacemacs-layers/d12frosted/packages.el
+++ emacs/spacemacs-layers/d12frosted/packages.el
@@ -10,12 +10,34 @@
 ;;; License: MIT
 
 (defvar d12frosted-packages
-  '(2048-game)
+  '(elfeed
+    elfeed-web
+    2048-game)
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
 (defvar d12frosted-excluded-packages '()
   "List of packages to exclude.")
+
+(defun d12frosted/init-elfeed-web ()
+  "Initialize elfeed-web package"
+  (use-package elfeed-web
+    :defer t
+    :init
+    :config))
+
+(defun d12frosted/init-elfeed ()
+  "Initialize elfeed package."
+  (use-package elfeed
+    :defer t
+    :init
+    :config
+    (evil-leader/set-key "oe" 'elfeed)
+    (setq elfeed-feeds '(("http://www.reddit.com/r/haskelltil/.rss" haskell reddit)
+                         ("http://www.reddit.com/r/haskell/.rss" haskell reddit)
+                         ("http://www.reddit.com/r/orgmode/.rss" emacs org-mode reddit)
+                         ("http://planet.haskell.org/rss20.xml" haskell)
+                         ("http://nullprogram.com/feed/" emacs)))))
 
 (defun d12frosted/init-2048-game ()
   "Initialize 2048-game package."
