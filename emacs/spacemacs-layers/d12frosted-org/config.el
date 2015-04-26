6d4fceb4ee8dccfb62248de559893e82398a0c0d
d12frosted <d12frosted@icloud.com>
2015-04-26 09:31:03 +0300

    improve d12frosted-org layer documentation
    * better documentation for configurable variables from config.el file
    * add README file

--- emacs/spacemacs-layers/d12frosted-org/config.el
+++ emacs/spacemacs-layers/d12frosted-org/config.el
@@ -14,26 +14,36 @@
 (defvar d12frosted/org-home-path "~/org/"
   "Directory where org files are stored.
 
-This directory is recursively searched for org files.
+   All org files in this directory and all subdirectories will be used as agenda
+   files for org agenda. If you want to ignore some files, checkout
+   'd12frosted/org-agenda-ignore-dirs variable. Currently you can ignore files
+   only by putting them into ignored directory.
 
-Don't forget to add trailing slash at the end.")
+   Warning! Don't forget to add trailing slash at the end.")
 
 (defvar d12frosted/org-agenda-ignore-dirs
   '("temporary/"
     "tmp/")
-  "List of directories to be ignored for agenda. Every path should be relative to d12frosted/org-home-path and should contain trailing slash.")
+  "List of directories to be ignored for agenda. Every path should be relative
+   to d12frosted/org-home-path and must contain trailing slash.")
 
 (defvar d12frosted/org-time-format
+  "Format for 'd12frosted/org-insert-time function."
   "%H:%M:%S")
 
 (defvar d12frosted/org-date-format
+  "Format for 'd12frosted/org-insert-date function."
   "%d %B %Y, %A")
 
 (defvar d12frosted/org-default-title
+  "Default title for org files. Is used by 'd12frosted/org-guess-title
+   when it failt to get the ttitle from buffer name."
   "Yet another org file")
 
 (defvar d12frosted/org-author-name
+  "Author name (used in 'd12frosted/org-new-file-template function.)"
   "John Doe")
 
 (defvar d12frosted/org-author-email
+  "Author email (used in 'd12frosted/org-new-file-template function.)"
   "example@domain.com")
