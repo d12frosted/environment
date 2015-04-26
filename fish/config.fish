06055fdf61923bbfe41948ee366dc615bde4588e
d12frosted <d12frosted@icloud.com>
2015-04-26 10:03:57 +0300

    add git configurations
    * remove setting git alias from config.fish file
    * add .gitignore_global file
    * add script for configuring git
    * update install configuration

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,3 @@
 set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/.bin $HOME/.rvm/bin
 set -x EDITOR "emacsclient"
 set fish_greeting
-
-# git
-
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)<%an> -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
