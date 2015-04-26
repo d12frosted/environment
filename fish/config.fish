29821a5cc8f4ec819c071769945e2f05647e8ed1
d12frosted <d12frosted@gmail.com>
2015-04-26 09:41:25 +0300

    remove unused aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,3 @@ set fish_greeting
 # git
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)<%an> -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
-
-# aliases
-
-alias e "emacs -nw"
-alias ee "perl ~/.environment/emacs_starter.pl"
-alias marked "open -a '/Applications/Marked 2.app'"
