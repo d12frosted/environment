4ef14e7493f8c0d5f2841278fbe6a1b99b686a4f
d12frosted <d12frosted@gmail.com>
2015-04-27 18:14:57 +0300

    update mu4e configurations
    * group notifications by id
    * change gendalfs behaviour based on caller
    * add more configurations for mu4e

--- email/gendalf.sh
+++ email/gendalf.sh
@@ -1,27 +1,72 @@
 #!/bin/sh
 
-MAILDIR="~/.mail"
+# todo - use MAILDIR variable
 
-gendalf_search () {
-    for d in $(find ~/.mail -type d -and -name "INBOX" -or -name "ARCHIVE"); do
-        for f in $(find $d -mmin -1 -and -type f); do
+# Speak, friend, and enter.
+
+FRIEND=$1
+INTERVAL=$2
+
+# but I speak only orcish
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+# gendalf activities
+
+gendalf_think () {
+    mbsync -a
+}
+
+gendalf_check () {
+    for d in $(find ~/.mail -type d -and -name "Inbox" -or -name "Archive"); do
+        for f in $(find $d -mmin -$INTERVAL -and -type f); do
             subject=$(mu view $f | grep '^Subject: ' | sed 's/^Subject: //')
             from=$(mu view $f | grep '^From: ' | sed 's/^From: //')
             if ! [ -z "$from" ]; then
-                gendalf_msg "$from" "$subject"
+                gendalf_mutter "$from" "$subject" "$f"
             fi
         done
     done
 }
 
-gendalf_msg () {
+gendalf_mutter () {
     terminal-notifier -message "Subject: $2" \
                       -subtitle "From: $1" \
                       -title "New mail" \
                       -sender "org.gnu.Emacs" \
-                      -activate "org.gnu.Emacs"
+                      -activate "org.gnu.Emacs" \
+                      -group "$3"
 }
 
-mbsync -a
-gendalf_search
-mu index -m ~/.mail
+gendalf_enter () {
+    mu index -m ~/.mail/
+}
+
+# gendalf figths
+
+echo '****' $(date "+%Y-%m-%d% %H:%M:%S")
+
+case $FRIEND in
+    "mu4e")
+        echo "     hello mu4e"
+        gendalf_think
+        gendalf_check
+        ;;
+
+    "saruman")
+        echo "     hello saruman"
+        gendalf_enter && gendalf_think && gendalf_check && gendalf_enter
+        ;;
+
+    "friend")
+        echo "     the gate opens slowly"
+        gendalf_check
+        ;;
+
+    *)
+        ;;
+esac
+
+echo
