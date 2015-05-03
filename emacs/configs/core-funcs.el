01cea704fe6579c45977a5b356c287f4fcb51cc2
d12frosted <d12frosted@gmail.com>
2015-05-03 20:39:16 +0300

    implement some plist functions

--- emacs/configs/core-funcs.el
+++ emacs/configs/core-funcs.el
@@ -42,6 +42,41 @@
                          dirs)))
     dirs))
 
+;;; plist functions
+;; =================
+
+(defun d12/mplist-get (plist prop)
+  "Get the values associated to PROP in PLIST, a modified plist.
+A modified plist is one where keys are keywords and values are
+all non-keywords elements that follow it.
+If there are multiple properties with the same keyword, only the first property
+and its values is returned.
+Currently this function infloops when the list is circular."
+  (let ((tail plist)
+        result)
+    (while (and (consp tail) (not (eq prop (car tail))))
+      (pop tail))
+    ;; pop the found keyword
+    (pop tail)
+    (while (and (consp tail) (not (keywordp (car tail))))
+      (push (pop tail) result))
+    (nreverse result)))
+
+(defun d12/mplist-remove (plist prop)
+  "Return a copy of a modified PLIST without PROP and its values.
+If there are multiple properties with the same keyword, only the first property
+and its values are removed."
+  (let ((tail plist)
+        result)
+    (while (and (consp tail) (not (eq prop (car tail))))
+      (push (pop tail) result))
+    (when (eq prop (car tail))
+      (pop tail)
+      (while (and (consp tail) (not (keywordp (car tail))))
+        (pop tail)))
+    (while (consp tail)
+      (push (pop tail) result))
+    (nreverse result)))
 ;;; Various stuff
 ;; ===============
 
