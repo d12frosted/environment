82fa21ec038fc432056e04e1bf5c46ed5476429a
d12frosted <d12frosted@gmail.com>
2015-05-11 18:50:17 +0300

    remove spacemacs configs

--- emacs/spacemacs-layers/mu4e/extensions/mu4e/mu4e-meta.el
+++
@@ -1,8 +0,0 @@
-;; auto-generated
-(defconst mu4e-mu-version "0.9.11"
-  "Required mu binary version; mu4e's version must agree with this.")
-
-(defconst mu4e-builddir "/private/tmp/mu-HPHXct/mu-0.9.11"
-  "Top-level build directory.")
-
-(provide 'mu4e-meta)
