95a5d36f6f1052895b3e2ff8f24f89f1ebc7954d
d12frosted <d12frosted@icloud.com>
2015-05-29 08:00:37 +0300

    add function to rename file and buffer

--- emacs/configs/core-funcs.el
+++ emacs/configs/core-funcs.el
@@ -48,6 +48,24 @@
     (insert-file-contents filePath)
     (buffer-string)))
 
+;; from magnars
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
 ;;; Buffers
 ;; =========
 
