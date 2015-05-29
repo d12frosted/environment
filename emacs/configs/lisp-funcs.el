6f92240177a717d1446a6477f82eeb19787bc15c
d12frosted <d12frosted@icloud.com>
2015-05-29 07:00:29 +0300

    eval and replace

--- emacs/configs/lisp-funcs.el
+++ emacs/configs/lisp-funcs.el
@@ -29,6 +29,17 @@
       (eval-region (min (point) (mark)) (max (point) (mark)))
     (eval-last-sexp prefix)))
 
+;; from magnars
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 ;; thanks to Chris Done
 (defun d12/paredit-kill-sexp ()
   "Kill the sexp at point."
