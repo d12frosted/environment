0583cbdd81f5ec6c2295adcd5df913f0132b17f8
d12frosted <d12frosted@gmail.com>
2015-05-31 08:43:42 +0300

    remove custom functions to move by words

--- emacs/configs/core-funcs.el
+++ emacs/configs/core-funcs.el
@@ -174,42 +174,6 @@ point reaches the beginning or end of the buffer, stop there."
     (when (= orig-point (point))
       (move-beginning-of-line 1))))
 
-(defun util-forward-word ()
-  "Move one word forward. Leave the pointer at start of word
-  instead of emacs default end of word. Treat _ as part of word"
-  (interactive)
-  (let ((start (point)) boundary at-boundary jump)
-    (setq at-boundary (eolp))
-    (setq boundary (line-end-position))
-    (forward-char 1)
-    (backward-word 1)
-    (forward-word 2)
-    (backward-word 1)
-    (backward-char 1)
-    (cond ((or (looking-at "_") (looking-at "\\.[0-9]\\.")) (forward-char 1)
-           (util-forward-word))
-          (t (forward-char 1)))
-    (if (and (not at-boundary) (> (point) boundary)) (goto-char boundary))
-    (setq jump (util-count-lines start (point)))
-    (if (> jump 0) (progn (forward-line (- 1 jump)) (back-to-indentation)))))
-
-(defun util-backward-word ()
-  "Move one word backward. Leave the pointer at start of word
-  Treat _ as part of word."
-  (interactive)
-  (let ((start (point)) boundary at-boundary jump)
-    (setq at-boundary (empty-line-prefix))
-    (setq boundary (line-beginning-position))
-    (backward-word 1)
-    (backward-char 1)
-    (cond ((or (looking-at "_") (looking-at "\\.[0-9]\\."))
-           (util-backward-word))
-          (t (forward-char 1)))
-    (if (and (not at-boundary) (< (point) boundary))
-        (progn (goto-char boundary) (back-to-indentation)))
-    (setq jump (util-count-lines start (point)))
-    (if (> jump 0) (progn (forward-line (- jump 1)) (end-of-line)))))
-
 ;;; Mode renaming and diminishing
 ;; ===============================
 
