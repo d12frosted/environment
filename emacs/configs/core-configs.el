e070937812e00dd55ff9a4d9186f8a044c177878
d12frosted <d12frosted@gmail.com>
2015-06-09 11:16:05 +0300

    use corral package

--- emacs/configs/core-configs.el
+++ emacs/configs/core-configs.el
@@ -491,6 +491,16 @@
 
 (use-package smartparens
   :ensure t)
+(use-package corral
+  :ensure t
+  :bind
+  ("M-9"  . corral-parentheses-backward)
+  ("M-0"  . corral-parentheses-forward)
+  ("M-["  . corral-brackets-backward)
+  ("M-]"  . corral-brackets-forward)
+  ("M-{"  . corral-braces-backward)
+  ("M-}"  . corral-braces-forward)
+  ("M-\"" . corral-double-quotes-backward))
 
 (use-package restclient                ; ReST REPL for Emacs
   :ensure t
