2ee124292623aefd57ab91c8e0a91e842a7a3df3
d12frosted <d12frosted@gmail.com>
2015-06-11 11:35:16 +0300

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,24 +1,45 @@
-function fish_prompt --description 'd12frosted prompt'
+function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
 
   __prompt_duration
+
+  echo
+
+  # Time
+  set_color $fish_color_comment
+  echo -ns "[" (date +%X) "] "
+
+  # User
+  set_color $fish_color_user
+  echo -n (whoami)
+  set_color normal
+
+  echo -n '@'
+
+  # Host
+  set_color $fish_color_host
+  echo -n (hostname -s)
+  set_color normal
+
+  echo -n ':'
 
   # PWD
   set_color $fish_color_cwd
   echo -n (prompt_pwd)
   set_color normal
 
+  __fish_venv
   __terlar_git_prompt
   __fish_hg_prompt
-  # echo
+  echo
 
   if not test $last_status -eq 0
     set_color $fish_color_error
-  else
-    set_color green
   end
 
-  echo -n ' λ '
+  # echo -n '➤ '
+  set_color $fish_color_operator
+  echo -n 'λ '
   set_color normal
 end
 
@@ -34,5 +55,11 @@ function __prompt_duration
         echo -es 'Finished in ' $CMD_DURATION ' ms' | terminal-notifier
       end
     end
+  end
+end
+
+function __fish_venv
+  if set -q VIRTUAL_ENV
+    echo -n -s "|" (set_color $fish_color_comment) (basename "$VIRTUAL_ENV") (set_color normal)
   end
 end
