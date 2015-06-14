367b28f0e790e2ddf9b575a205bd91f28a7e5082
d12frosted <d12frosted@icloud.com>
2015-06-14 12:49:13 +0300

    spacemacs

---
+++ spacemacs/d12frosted/config.el
@@ -0,0 +1,49 @@
+;;; config.el --- d12frosted Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Boris Buliga & Contributors
+;;
+;; Author: Boris Buliga <d12frosted@icloud.com>
+;; URL: https://github.com/d12frosted/environment
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar d12/org-home-path "~/org/"
+  "Directory where org files are stored.
+   All org files in this directory and all subdirectories will be used as agenda
+   files for org agenda. If you want to ignore some files, checkout
+   'd12/org-agenda-ignore-dirs variable. Currently you can ignore files
+   only by putting them into ignored directory.
+   Warning! Don't forget to add trailing slash at the end.")
+
+(defvar d12/org-agenda-ignore-dirs
+  '("temporary/"
+    "tmp/")
+  "List of directories to be ignored for agenda. Every path should be relative
+   to d12/org-home-path and must contain trailing slash.")
+
+(defvar d12/org-time-format
+  "%H:%M:%S"
+  "Format for 'd12/org-insert-time function.")
+
+(defvar d12/org-date-format
+  "%d %B %Y, %A"
+  "Format for 'd12/org-insert-date function.")
+
+(defvar d12/org-default-title
+  "Yet another org file"
+  "Default title for org files. Is used by 'd12/org-guess-title
+   when it failt to get the ttitle from buffer name.")
+
+(defvar d12/org-author-name
+  user-full-name
+  "Author name (used in 'd12/org-new-file-template function.)")
+
+(defvar d12/org-author-email
+  user-mail-address
+  "Author email (used in 'd12/org-new-file-template function.)")
+
+(spacemacs/declare-prefix "." "org")
+(spacemacs/declare-prefix ".i" "insert")
---
+++ spacemacs/d12frosted/keybindings.el
@@ -0,0 +1,21 @@
+;;; keybindings.el --- d12frosted Layer keybindings File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Boris Buliga & Contributors
+;;
+;; Author: Boris Buliga <d12frosted@icloud.com>
+;; URL: https://github.com/d12frosted/environment
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(bind-keys
+ ("M-g g"           . d12/goto-line-and-center)
+ ("C-w"             . d12/cut-line-or-region)
+ ("M-w"             . d12/copy-line-or-region)
+ ("C-S-<backspace>" . d12/delete-line-or-region)
+ ("C-c u d"         . d12/duplicate-line-or-region)
+ ("C-;"             . comment-dwim)
+ ("M-;"             . endless/comment-line-or-region)
+ )
