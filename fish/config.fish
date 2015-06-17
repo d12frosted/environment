543f0b70f027d79ddb919b511ac3da1d13e5dc5f
d12frosted <d12frosted@gmail.com>
2015-06-17 13:22:36 +0300

    nix env

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,8 @@ set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/
 set -x EDITOR "emacsclient"
 set fish_greeting
 
+# nix
+eval (~/.environment/fish/nix_fish_env.sh) 2>/dev/null
+
+# python
 eval (python -m virtualfish)
