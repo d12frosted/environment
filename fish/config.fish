3dd9a7905ddf8a5ebfc4fb494f68a2afcd73b5b2
d12frosted <d12frosted@gmail.com>
2015-06-20 12:34:40 +0300

    update PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 # variables
 
-set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/.bin $HOME/.rvm/bin
+set -x PATH $HOME/.cabal/bin /usr/local/bin /bin /usr/sbin /sbin /usr/bin $HOME/.bin $HOME/.local/bin $HOME/.rvm/bin
 set -x EDITOR "emacsclient"
 set fish_greeting
 
