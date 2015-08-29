08f48757f8705cc3b5bc111cb9c8a5f2628d9213
d12frosted <d12frosted@icloud.com>
2015-08-29 14:22:44 +0300

    remove unused fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,8 @@
 # variables
 
 set -x PATH $HOME/.local/bin $PATH
-# set -x MANPATH ""
 set -x EDITOR "emacsclient"
 set fish_greeting ""
-
-# haskell
-# set -x PATH $HOME/.stack/programs/x86_64-osx/ghc-7.10.2/bin $PATH
 
 # theme
 set fish_color_autosuggestion "8e908c"
@@ -17,13 +13,3 @@ set fish_color_error "c82829"
 set fish_color_param "4271ae"
 set fish_color_quote "718c00"
 set fish_color_redirection "3e999f"
-
-# nix
-# eval (~/.environment/fish/nix_fish_env.sh) 2>/dev/null
-
-# git subrepo
-# set -x PATH $HOME/Developer/git-subrepo/lib $PATH
-# set -gx MANPATH $MANPATH $HOME/Developer/git-subrepo/man
-
-# python
-# eval (python -m virtualfish)
