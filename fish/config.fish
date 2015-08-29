114527f05a2f498447e5aaca37c93e3c7be97be3
d12frosted <d12frosted@gmail.com>
2015-08-29 14:22:59 +0300

    update fish colors

--- fish/config.fish
+++ fish/config.fish
@@ -5,11 +5,12 @@ set -x EDITOR "emacsclient"
 set fish_greeting ""
 
 # theme
-set fish_color_autosuggestion "8e908c"
-set fish_color_command "8959a8"
-set fish_color_comment "eab700"
+set fish_color_command blue
+set fish_color_param "715ab1"
+set fish_color_autosuggestion "9f8fbd"
+set fish_color_operator "428374"
 set fish_color_end "8959a8"
-set fish_color_error "c82829"
-set fish_color_param "4271ae"
+set fish_color_error "e0211d"
+set fish_color_comment "2aa198"
 set fish_color_quote "718c00"
 set fish_color_redirection "3e999f"
