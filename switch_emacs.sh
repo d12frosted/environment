3bcddf0cfbe1af851b9b745654c845e9837d07c6
d12frosted <d12frosted@gmail.com>
2015-09-02 20:33:46 +0300

    update script for switching emacs.d dirs

--- switch_emacs.sh
+++ switch_emacs.sh
@@ -2,18 +2,24 @@
 
 case $1 in
     "d12")
-	      echo "d12"
-	      rm -rf ~/.emacs.d
-	      ln -s ~/.environment/emacs ~/.emacs.d
+        emacs_path=~/.environment/emacs
 	      ;;
 
     "spacemacs")
-	      echo "spacemacs"
-	      rm -rf ~/.emacs.d
-	      ln -s ~/.emacs.d.spacemacs ~/.emacs.d
+	      emacs_path=~/.emacs.d.spacemacs
 	      ;;
+
+    "purcell")
+	      emacs_path=~/.emacs.d.purcell
+	      ;;
+
     *)
 	      echo "unknown type"
 	      exit 1
 	      ;;
 esac
+
+echo "Switching from '$(readlink ~/.emacs.d)'
+            to '$emacs_path'"
+rm -rf ~/.emacs.d
+ln -s $emacs_path ~/.emacs.d
