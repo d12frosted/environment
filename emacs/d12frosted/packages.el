02a1a7aa5efedca164be29654f3f33aeeb246863
d12frosted <d12frosted@icloud.com>
2015-09-08 16:33:29 +0300

    enable d12-haskell layer

--- emacs/d12frosted/packages.el
+++ emacs/d12frosted/packages.el
@@ -17,9 +17,6 @@
         org
         org-journal
         omnisharp
-        haskell-mode
-        shm
-        flycheck-haskell
         ))
 
 ;; List of packages to exclude.
@@ -422,112 +419,6 @@ If not, show simply the clocked time like 01:50."
               )
             hs-special-modes-alist)))
   )
-
-(defun d12frosted/init-haskell-mode ()
-  "Initialize haskell-mode."
-  (use-package haskell-mode
-    :defer t
-    :config
-    (progn
-      (defun d12/init-haskell-mode ()
-        (setq global-hl-line-mode nil)
-        (if (fboundp 'electric-indent-local-mode)
-            (electric-indent-local-mode -1)))
-
-      (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-      (add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
-      (add-hook 'haskell-mode-hook 'd12/init-haskell-mode)
-
-      (spacemacs|diminish interactive-haskell-mode "λ")
-
-      (setq haskell-process-type 'stack-ghci
-            haskell-notify-p t
-            haskell-tags-on-save nil
-            haskell-interactive-popup-error nil
-            haskell-process-suggest-remove-import-lines nil
-            haskell-process-auto-import-loaded-modules t
-            haskell-stylish-on-save nil
-            haskell-process-log t
-            haskell-process-reload-with-fbytecode nil
-            haskell-process-use-presentation-mode t
-            haskell-interactive-mode-include-file-name nil
-            haskell-interactive-mode-eval-pretty nil
-            haskell-process-suggest-haskell-docs-imports nil
-            hindent-style "chris-done"
-            haskell-interactive-mode-eval-mode 'haskell-mode)
-      )
-
-    (evil-leader/set-key-for-mode 'haskell-mode
-        "mgg"  'haskell-mode-jump-to-def-or-tag
-        "mf"   'haskell-mode-stylish-buffer
-
-        "msb"  'haskell-process-load-or-reload
-        "msc"  'haskell-interactive-mode-clear
-        "mss"  'haskell-interactive-bring
-        "msS"  'haskell-interactive-switch
-
-        "mca"  'haskell-process-cabal
-        "mcb"  'haskell-process-cabal-build
-        "mcc"  'haskell-compile
-        "mcv"  'haskell-cabal-visit-file
-
-        "mhd"  'inferior-haskell-find-haddock
-        "mhh"  'hoogle
-        "mhi"  'haskell-process-do-info
-        "mht"  'haskell-process-do-type
-        "mhT"  'spacemacs/haskell-process-do-type-on-prev-line
-        "mhy"  'hayoo
-
-        "mdd"  'haskell-debug
-        "mdb"  'haskell-debug/break-on-function
-        "mdn"  'haskell-debug/next
-        "mdN"  'haskell-debug/previous
-        "mdB"  'haskell-debug/delete
-        "mdc"  'haskell-debug/continue
-        "mda"  'haskell-debug/abandon
-        "mdr"  'haskell-debug/refresh)
-
-      ;; Switch back to editor from REPL
-      (evil-leader/set-key-for-mode 'haskell-interactive-mode
-        "msS"  'haskell-interactive-switch)
-
-      ;; Compile
-      (evil-leader/set-key-for-mode 'haskell-cabal
-        "mC"  'haskell-compile)
-
-      ;; Cabal-file bindings
-      (evil-leader/set-key-for-mode 'haskell-cabal-mode
-        ;; "m="  'haskell-cabal-subsection-arrange-lines ;; Does a bad job, 'gg=G' works better
-        "md" 'haskell-cabal-add-dependency
-        "mb" 'haskell-cabal-goto-benchmark-section
-        "me" 'haskell-cabal-goto-executable-section
-        "mt" 'haskell-cabal-goto-test-suite-section
-        "mm" 'haskell-cabal-goto-exposed-modules
-        "ml" 'haskell-cabal-goto-library-section
-        "mn" 'haskell-cabal-next-subsection
-        "mp" 'haskell-cabal-previous-subsection
-        "mN" 'haskell-cabal-next-section
-        "mP" 'haskell-cabal-previous-section
-        "mf" 'haskell-cabal-find-or-create-source-file)
-
-      ;; Make "RET" behaviour in REPL saner
-      (evil-define-key 'insert haskell-interactive-mode-map
-        (kbd "RET") 'haskell-interactive-mode-return)
-      (evil-define-key 'normal haskell-interactive-mode-map
-        (kbd "RET") 'haskell-interactive-mode-return)))
-
-(defun d12frosted/init-shm ()
-  "Initialize structured haskell mode."
-  (use-package shm
-    :init
-    (add-hook 'haskell-mode-hook 'structured-haskell-mode)))
-
-(defun d12frosted/init-flycheck-haskell ()
-  (use-package flycheck-haskell
-    :if (configuration-layer/package-usedp 'flycheck)
-    :commands flycheck-haskell-configure
-    :init
-    (add-hook 'flycheck-mode-hook 'flycheck-haskell-configure)))
 
 ;; For each package, define a function d12frosted/init-<package-d12frosted>
 ;;
