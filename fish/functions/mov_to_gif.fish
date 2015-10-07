8fe0a59d1bf3b9b4b5026c04b1944679ac4658d3
d12frosted <d12frosted@icloud.com>
2015-10-07 10:10:31 +0300

    add ability to scale output gif in mov_to_gif

--- fish/functions/mov_to_gif.fish
+++ fish/functions/mov_to_gif.fish
@@ -1,4 +1,9 @@
-function mov_to_gif -a file -d "Convert .mov file to .gif"
+function mov_to_gif -a file scale -d "Convert video to gif"
+  if test ! -f $file
+    echo "You must provide input video file as first argument"
+    return 1
+  end
+
   # process input
   set -l out (echo $file | sed 's/\(.*\)\..*/\1.gif/')
 
@@ -6,6 +11,10 @@ function mov_to_gif -a file -d "Convert .mov file to .gif"
   __package_install ffmpeg
   __package_install gifsicle
 
+  if test -z $scale
+    set scale 1
+  end
+
   # just do the work
-  ffmpeg -i $file -pix_fmt rgb8 -r 24 -f gif - | gifsicle --optimize=3 --delay=3 > $out
+  ffmpeg -i $file -vf "scale=iw*$scale:ih*$scale" -pix_fmt rgb8 -r 24 -f gif - | gifsicle --optimize=3 --delay=3 > $out
 end
