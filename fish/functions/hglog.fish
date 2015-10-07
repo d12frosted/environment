e355731d0cf8a48ceba04833b95cf063462a32aa
d12frosted <d12frosted@gmail.com>
2015-10-07 09:36:57 +0300

    add description to hglog

--- fish/functions/hglog.fish
+++ fish/functions/hglog.fish
@@ -1,4 +1,4 @@
-function hglog
+function hglog -d "Nice and short mercurial log"
   set -l template (echo -es '\t\033[38;5;' $green '{node|short} \033[38;5;' $purple '{fill(sub("^(.{10}).*$","\\1",branch),"10")} \033[38;5;' $yellow '{date|isodatesec} \033[38;5;' $red '{author|user}: \033[38;5;' $normal '{desc|strip|firstline}\033[m\n')
 
   set -l highlight (echo -e '\033[48;5;19m\033[38;5;256m')
