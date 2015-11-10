7943587966deb30950ef4b1a382bd4a5acdbdc0e
d12frosted <d12frosted@gmail.com>
2015-11-10 16:06:07 +0200

    add support of private fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,18 @@
-# variables
+set -l PRIVATE_FISH_CONFIGS_HOME $HOME/Dropbox/Apps/fish
 
+# private pre-configs
+if test -f $PRIVATE_FISH_CONFIGS_HOME/preconfig.fish
+  source $PRIVATE_FISH_CONFIGS_HOME/preconfig.fish
+end
+
+# variables
 set -x XDG_CONFIG_HOME ~/.environment
 set -x SPACEMACSDIR $XDG_CONFIG_HOME/emacs
 set -x PATH $HOME/.local/bin /usr/texbin $PATH
 set -x EDITOR "emacsclient"
 set fish_greeting ""
 
+# aliases
 alias ghci "stack ghci"
 alias e "emacsclient"
 alias en "emacsclient -nw"
@@ -15,3 +22,8 @@ __set_theme "tomorrow_night"
 
 # python
 eval (python -m virtualfish)
+
+# private post-configs
+if test -f $PRIVATE_FISH_CONFIGS_HOME/postconfig.fish
+  source $PRIVATE_FISH_CONFIGS_HOME/postconfig.fish
+end
