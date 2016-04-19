42fa9243153502aed1b7664ce9e79c8a8c3005ad
d12frosted <d12frosted@gmail.com>
2016-04-19 14:20:00 +0300

    [git] add pushall alias

--- git/configure.sh
+++ git/configure.sh
@@ -13,5 +13,8 @@ git config --global core.editor emacsclient
 # pretty lg alias
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)<%an> -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
 
+# push all commits to current branch and push tags to all remotes
+git config --global alias.pushall '!for r in $(git remote); do git push $r $(git symbolic-ref --short HEAD); done && for r in $(git remote); do git push --tags; done'
+
 # setup push method
 git config --global push.default simple
