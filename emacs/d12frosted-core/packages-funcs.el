71a771265d32f5ad1e43ed5b9edb220c02a10c7b
d12frosted <d12frosted@gmail.com>
2016-04-28 18:40:02 +0300

    [emacs] update d12frosted-core layer organisation

---
+++ emacs/d12frosted-core/packages-funcs.el
@@ -0,0 +1,17 @@
+;;; packages-funcs.el --- d12frosted-core layer packages-funcs file for
+;;; Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Boris Buliga <d12frosted@d12frosted.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+;; packages-funcs.el ends here
