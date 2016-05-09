0b3a0d7fe3ed8e65e2b27c99a24a2c4602b89356
d12frosted <d12frosted@gmail.com>
2016-05-09 13:01:59 +0300

    [emacs] add emacs header template

---
+++ emacs/snippets/emacs-lisp-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `emacs-lisp-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'emacs-lisp-mode
+                     '(("d12-header" ";;; `(file-name-nondirectory (buffer-file-name))` --- $1\n\n;; Copyright (c) `(format-time-string \"%Y\")` Boris Buliga\n\n;; Author: `d12/full-name` <`d12/email`>\n;; Maintainer: `d12/full-name` <`d12/email`>\n;; Created: `(format-time-string \"%d %b %Y\")`\n\n;; Keywords:\n;; Homepage:\n\n;; Package-Version: 0.0.1\n;; Package-Requires: ()\n\n;; This file is not part of GNU Emacs.\n;;; License: GPLv3\n\n;;; Commentary:\n;;\n\n;;; Code:\n;;\n\n$0\n\n;;; `(file-name-nondirectory (buffer-file-name))` ends here\n" "d12-emacs-lisp-header" nil nil nil "/Users/d12frosted/.environment/emacs/snippets/emacs-lisp-mode/d12-header" nil nil)))
+
+
+;;; Do not edit! File generated at Mon May  9 12:59:04 2016
