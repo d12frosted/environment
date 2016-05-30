4cf2193c3c005841727940ce32ba304f8d0171f3
d12frosted <d12frosted@gmail.com>
2016-05-30 07:36:18 +0300

    [emacs] update d12frosted-core structure
    Follow up recent changes in layers system

--- emacs/d12frosted-core/funcs.el
+++ emacs/d12frosted-core/funcs.el
@@ -46,8 +46,6 @@ string before that."
       (string-to-number string)
     default))
 
-;;; Miscellaneous
-
 (defun d12/get-env-shell-type ()
   (interactive)
   (let* ((path (getenv "SHELL"))
@@ -106,5 +104,87 @@ expression."
          (p1 (if has-region start (point-min)))
          (p2 (if has-region end (point-max))))
     (fill-individual-paragraphs p1 p2)))
+
+(defun d12-github//get-remote-relative-url (remote)
+  "Return relative url in form $user/$repo for a given remote. It
+works only for github urls (http[s] and ssh)."
+  (let ((url (vc-git--run-command-string nil "config" (format "remote.%s.url" remote))))
+    (unless url (error "Not in a GitHub repo"))
+    (when (and url (string-match "github.com:?/?\\(.*\\)" url))
+      (replace-regexp-in-string "\\.git$" "" (match-string 1 url)))))
+
+(defun d12-github//get-remote-branch (remote)
+  "Return the name of remote branch current branch is tracking.
+If there is none return 'master'."
+  (let* ((ref (replace-regexp-in-string
+               "\n" ""
+               (vc-git--run-command-string nil "symbolic-ref" "-q" "HEAD")))
+         (origin-branch (replace-regexp-in-string
+                         "\n" ""
+                         (vc-git--run-command-string
+                          nil "for-each-ref" "--format=%(upstream:short)" ref)))
+         (branch-name (mapconcat 'identity
+                                 (cdr (split-string origin-branch "/"))
+                                 "/")))
+    (if (eq branch-name "") "master" branch-name)))
+
+(defun d12-github/browse-remote ()
+  "Browse remote repository."
+  (interactive)
+  (let* ((remotes (magit-list-remotes))
+         (remote (if (eq (length remotes) 1)
+                     (first remotes);
+                   (completing-read "Choose remote: " remotes)))
+         (relative-url (d12-github//get-remote-relative-url remote))
+         (branch (d12-github//get-remote-branch remote))
+         (url (format "https://github.com/%s/tree/%s" relative-url branch)))
+    (browse-url url)))
+
+(when (configuration-layer/package-usedp 'projectile)
+  (defun d12/projectile-replace-regexp ()
+    "Replace a string in the project using `tags-query-replace'.
+Less efficient than `projectile-replace' but at least allows
+usage of regular expressions. See
+https://github.com/bbatsov/projectile/issues/576 for more details
+on `projectile-replace' issue with regexps."
+    (interactive "P")
+    (let* ((old-text (read-string
+                      (projectile-prepend-project-name "Replace: ")
+                      (projectile-symbol-or-selection-at-point)))
+           (new-text (read-string
+                      (projectile-prepend-project-name
+                       (format "Replace %s with: " old-text))))
+           (files (-map (lambda (f) (concat (projectile-project-root) f)) (projectile-current-project-files))))
+      (tags-query-replace old-text new-text nil (cons 'list files)))))
+
+(when (configuration-layer/layer-usedp 'ivy)
+  (defun d12-ivy//delete-file (file)
+  "Action for `counsel-find-file' that removes selected FILE.
+
+It also kills corresponding buffer (if any exists) and
+invalidates projectile cache (if it's possible)."
+  (with-ivy-window
+    (when-let ((buffer (find-buffer-visiting file)))
+      (kill-buffer buffer))
+    (delete-file file)
+    (when (projectile-project-p)
+      (call-interactively #'projectile-invalidate-cache))))
+
+(defun d12-ivy//rename-file (file)
+  "Action for `counsel-find-file' that renames selected FILE.
+
+It also kills corresponding buffer (if any exists)"
+  (with-ivy-window
+    (let* ((short-name (file-name-nondirectory file))
+           (newfile (and short-name
+                     (read-file-name
+                      (format "Rename %s to: " short-name))))
+           (buffer (find-buffer-visiting file)))
+      (rename-file file newfile)
+      (when buffer
+        (kill-buffer buffer)
+        (find-file newfile))
+      (when (projectile-project-p)
+        (call-interactively #'projectile-invalidate-cache))))))
 
 ;;; funcs.el ends here
