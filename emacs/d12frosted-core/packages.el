b9c946a2993d4e6f9e55c70874438aebb6a4e3b2
d12frosted <d12frosted@gmail.com>
2016-06-04 09:54:37 +0300

    [emacs] update flyspell-correct

--- emacs/d12frosted-core/packages.el
+++ emacs/d12frosted-core/packages.el
@@ -331,12 +331,14 @@ Supports negative arguments and repeating."
 
 (defun d12frosted-core/init-flyspell-correct ()
   (use-package flyspell-correct
+    :commands (flyspell-correct-word-generic
+               flyspell-correct-previous-word-generic)
     :init
     (when (configuration-layer/layer-usedp 'ivy)
       (setq flyspell-correct-interface 'flyspell-correct-ivy))
     (when (configuration-layer/layer-usedp 'helm)
       (setq flyspell-correct-interface 'flyspell-correct-helm))
     (when (bound-and-true-p flyspell-correct-interface)
-      (spacemacs/set-leader-keys "Sc" 'flyspell-correct-word-generic))))
+      (spacemacs/set-leader-keys "Sc" 'flyspell-correct-previous-word-generic))))
 
 ;;; packages.el ends here
