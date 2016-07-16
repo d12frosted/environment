12692a9c01befc6aae4fe7dfe86ad4793981f8fd
d12frosted <d12frosted@gmail.com>
2016-07-16 13:23:58 +0300

    [emacs] setup hindent style

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -46,7 +46,9 @@
 
      ;; languages
      emacs-lisp
-     (haskell :variables haskell-completion-backend 'ghci)
+     (haskell :variables
+      haskell-completion-backend 'ghci
+      haskell-enable-hindent-style "chris-done")
      (html :variables
       web-mode-markup-indent-offset 2
       web-mode-attr-indent-offset 2
