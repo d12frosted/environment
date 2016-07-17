c9b2ab73112ac698125b211a94dd883c023a2670
d12frosted <d12frosted@gmail.com>
2016-07-17 11:24:51 +0300

    [emacs] move lua functions to funcs.el

--- emacs/d12frosted-lua/funcs.el
+++ emacs/d12frosted-lua/funcs.el
@@ -15,4 +15,34 @@
 ;;; Code:
 ;;
 
+(defun moai-run-main ()
+  "Run main.lua using moai."
+  (interactive)
+  (let* ((buffer-name d12frosted-lua-moai-buffer-name)
+         (buffer (get-buffer buffer-name))
+         (currentBuffer (current-buffer))
+         workdir)
+    (when buffer
+      (when-let ((window (get-buffer-window buffer)))
+        (delete-window window))
+      (kill-buffer buffer))
+    (setq buffer (generate-new-buffer buffer-name))
+    (if (projectile-project-p)
+        (let ((root (projectile-project-root)))
+          (projectile-save-project-buffers)
+          (setq workdir (concat root "lua"))
+          (async-shell-command (concat "cd " (shell-quote-argument workdir) "; and moai main.lua") buffer buffer))
+      (async-shell-command (concat "moai " (buffer-file-name)) buffer buffer))
+    (switch-to-buffer-other-window buffer)
+    (when workdir
+      (setq-local default-directory workdir))
+    (select-window (get-buffer-window currentBuffer))))
+
+(defun moai-upload ()
+  "Upload moai game to device."
+  (interactive)
+  (setq-local async-shell-command-buffer 'confirm-kill-process)
+  (if (projectile-project-p)
+      (bpr-spawn (concat "upload_moai_game '" (projectile-project-root) "/lua'"))))
+
 ;;; funcs.el ends here
