51fc069e9828d3d040ce9cf5b8da80a44d860c38
d12frosted <boris@d12frosted.io>
2016-10-04 23:20:23 +0300

    [emacs] setup some custom faces

--- emacs/d12frosted-visual/packages.el
+++ emacs/d12frosted-visual/packages.el
@@ -18,7 +18,8 @@
     beacon
     spaceline
     (d12-pretty-greek :location local)
-    all-the-icons))
+    all-the-icons
+    spacemacs-theme))
 
 (defun d12frosted-visual/init-beacon ()
   (use-package beacon
@@ -74,5 +75,16 @@
 
 (defun d12frosted-visual/init-all-the-icons ()
   (use-package all-the-icons))
+
+(defun d12frosted-visual/post-init-spacemacs-theme ()
+  (use-package spacemacs-theme
+    :defer t
+    :config
+    (custom-set-faces
+     '(whitespace-tab ((t (:foreground "#F6ECE4")))))
+    (custom-set-faces
+     '(org-column ((t (:background "#edf1ed")))))
+    (custom-set-faces
+     '(font-lock-preprocessor-face ((t (:background "#F3F3B2")))))))
 
 ;;; packages.el ends here
