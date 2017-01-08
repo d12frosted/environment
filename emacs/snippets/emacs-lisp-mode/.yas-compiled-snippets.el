f0922738c2a89f87db5a3c57c585385906cea3e2
d12frosted <boris@d12frosted.io>
2017-01-08 14:51:31 +0200

    [emacs] auto update snippets file

--- emacs/snippets/emacs-lisp-mode/.yas-compiled-snippets.el
+++ emacs/snippets/emacs-lisp-mode/.yas-compiled-snippets.el
@@ -5,4 +5,4 @@
                      '(("d12-header" ";;; `(file-name-nondirectory (buffer-file-name))` --- $1\n\n;; Copyright (c) `(format-time-string \"%Y\")` Boris Buliga\n\n;; Author: `d12/full-name` <`d12/email`>\n;; Maintainer: `d12/full-name` <`d12/email`>\n;; Created: `(format-time-string \"%d %b %Y\")`\n\n;; Keywords:\n;; Homepage:\n\n;; Package-Version: 0.0.1\n;; Package-Requires: ()\n\n;; This file is not part of GNU Emacs.\n;;; License: GPLv3\n\n;;; Commentary:\n;;\n\n;;; Code:\n;;\n\n$0\n\n;;; `(file-name-nondirectory (buffer-file-name))` ends here\n" "d12-emacs-lisp-header" nil nil nil "/Users/d12frosted/.environment/emacs/snippets/emacs-lisp-mode/d12-header" nil nil)))
 
 
-;;; Do not edit! File generated at Mon May  9 12:59:04 2016
+;;; Do not edit! File generated at Sun Oct  2 08:21:20 2016
