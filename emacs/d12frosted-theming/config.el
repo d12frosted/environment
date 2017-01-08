3cf131b0f2e179c1e6b5c44aea6e1a8e20524d17
d12frosted <boris@d12frosted.io>
2017-01-08 14:52:25 +0200

    [emacs] tune spacemacs-theme for org-mode

--- emacs/d12frosted-theming/config.el
+++ emacs/d12frosted-theming/config.el
@@ -18,6 +18,14 @@
  '((spacemacs-light
     ;; Font locking
     (font-lock-preprocessor-face :background "#F3F3B2")
-    (whitespace-tab :foreground "#F6ECE4"))))
+    (whitespace-tab :foreground "#F6ECE4")
+
+    ;; org-mode
+    (org-agenda-date :height 1.3)
+    (org-agenda-date-weekend :height 1.3)
+    (org-agenda-date-today :height 1.3
+                           :underline t)
+    (org-scheduled-today :height 1.0)
+    (org-agenda-done :height 1.0))))
 
 ;;; config.el ends here
