5de0173255e481ee6ac57247afbc1c29e0b38cd2
Boris the Golem <golem@d12frosted.io>
2017-07-30 14:49:02 +0300

    [emacs] use ghub in d12-org-capture-url instead of gh
    gh lacks some ends points I need (and there is no obvious and simple way for
    adding missing ones) and generally it's too much for such a simple task

--- emacs/spacemacs/d12-org/local/d12-org-capture-url/d12-org-capture-url.el
+++ emacs/spacemacs/d12-org/local/d12-org-capture-url/d12-org-capture-url.el
@@ -7,7 +7,7 @@
 ;;; URL: https://github.com/d12frosted/environment/emacs
 ;;
 ;; Package-Version: 0.0.1
-;; Package-Requires: ((gh "0.10.0"))
+;; Package-Requires: ((ghub "1.1.0"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;; License: GPLv3
@@ -16,26 +16,31 @@
 ;;
 ;;; Code:
 
-(require 'gh-issues)
+(require 'ghub)
 
 (defconst d12-org-capture-github-issue-regex
-  "\\(https://github.com/\\([[:alnum:]]+\\)/\\([[:alnum:]\-]+\\)/[[:alpha:]]+/\\([[:digit:]]+\\)\\).*")
+  "\\(https://github.com/\\([[:alnum:]]+\\)/\\([[:alnum:]\-]+\\)/issues/\\([[:digit:]]+\\)\\).*")
+
+(defconst d12-org-capture-github-pull-regex
+  "\\(https://github.com/\\([[:alnum:]]+\\)/\\([[:alnum:]\-]+\\)/pull/\\([[:digit:]]+\\)\\).*")
+
 
 (defun d12-org-capture-url (url)
   "Format url for capture template."
   (cond
-   ((string-match d12-org-capture-github-issue-regex url)
-    (d12-org-capture--github-issue
+   ((or (string-match d12-org-capture-github-issue-regex url)
+        (string-match d12-org-capture-github-pull-regex url))
+    (d12-org-capture-url--github-issue-or-pull
      (match-string 1 url)
      (match-string 2 url)
      (match-string 3 url)
      (match-string 4 url)))
    (t (format "%s" url))))
 
-(defun d12-org-capture--github-issue (url user repo issue)
-  (let* ((api (gh-issues-api "api" :sync nil :cache nil :num-retries 1))
-         (response (gh-issues-issue-get api user repo issue))
-         (title (oref (oref response data) title)))
+(defun d12-org-capture-url--github-issue-or-pull (url user repo issue)
+  (let* ((api (format "/repos/%s/%s/issues/%s" user repo issue))
+         (response (ghub-get api))
+         (title (alist-get 'title response)))
     (format "[[%s][%s/%s#%s]] - %s"
             url user repo issue title)))
 
