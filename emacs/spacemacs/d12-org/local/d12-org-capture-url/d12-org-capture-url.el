2bae3b03772657c0e6b4c691ba0f4dbfed34f9ee
Boris the Golem <golem@d12frosted.io>
2017-07-30 14:50:44 +0300

    [emacs] implement rule for capturing github commits

--- emacs/spacemacs/d12-org/local/d12-org-capture-url/d12-org-capture-url.el
+++ emacs/spacemacs/d12-org/local/d12-org-capture-url/d12-org-capture-url.el
@@ -24,6 +24,8 @@
 (defconst d12-org-capture-github-pull-regex
   "\\(https://github.com/\\([[:alnum:]]+\\)/\\([[:alnum:]\-]+\\)/pull/\\([[:digit:]]+\\)\\).*")
 
+(defconst d12-org-capture-github-commit-regex
+  "\\(https://github.com/\\([[:alnum:]]+\\)/\\([[:alnum:]\-]+\\)/commit/\\([[:alnum:]]+\\)\\).*")
 
 (defun d12-org-capture-url (url)
   "Format url for capture template."
@@ -31,6 +33,12 @@
    ((or (string-match d12-org-capture-github-issue-regex url)
         (string-match d12-org-capture-github-pull-regex url))
     (d12-org-capture-url--github-issue-or-pull
+     (match-string 1 url)
+     (match-string 2 url)
+     (match-string 3 url)
+     (match-string 4 url)))
+   ((string-match d12-org-capture-github-commit-regex url)
+    (d12-org-capture-url--github-commit
      (match-string 1 url)
      (match-string 2 url)
      (match-string 3 url)
@@ -43,6 +51,13 @@
          (title (alist-get 'title response)))
     (format "[[%s][%s/%s#%s]] - %s"
             url user repo issue title)))
+
+(defun d12-org-capture-url--github-commit (url user repo commit)
+  (let* ((api (format "/repos/%s/%s/commits/%s" user repo commit))
+         (response (ghub-get api))
+         (title (car (split-string (alist-get 'message (alist-get 'commit response)) "\n"))))
+    (format "[[%s][%s/%s#%s]] - %s"
+            url user repo commit title)))
 
 (provide 'd12-org-capture-url)
 
