bc77d3acd8e0e1838f0c80d6a486fa0206e8c8b3
Boris the Golem <golem@d12frosted.io>
2017-08-01 16:55:32 +0300

    [emacs] add missing init.el :smile_cat:

---
+++ emacs/init.el
@@ -0,0 +1,70 @@
+;;; init.el --- init file for personal configurations -*- lexical-binding: t; -*-
+;;
+;;; Copyright (c) 2015-2017 Boris Buliga
+;;
+;;; Author: Boris Buliga <boris@d12frosted.io>
+;;
+;;; URL: https://github.com/d12frosted/environment/emacs
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;; Naming conventions:
+;;
+;;   d12-...   public variables or non-interactive functions
+;;   d12--...  private anything (non-interactive), not safe for direct use
+;;   d12/...   an interactive function; safe for M-x or keybinding
+;;   d12|...   hook function
+;;   d12*...   advising functions
+;;
+;;; Code:
+
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
+(defconst package--initialized nil)
+
+;; setup emacs configuration
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+
+;; load some core features
+(require 'd12-path (concat user-emacs-directory "core/d12-path"))
+(require 'd12-spacemacs (concat user-emacs-directory "core/d12-spacemacs"))
+
+;; setup and load `custom-file'
+(setq custom-file d12-path-custom-file)
+(load custom-file t)
+
+;; load `private.el' file containing all the sensitive data
+(load (concat d12-path-emacs-private "private.el"))
+
+;; setup package-user-dir to allow seamless switch between emacs versions
+(setq package-user-dir
+      (file-name-as-directory
+       (concat d12-path-emacs-cache "elpa/" emacs-version)))
+
+;; load spacemacs
+;; (setq d12-path-spacemacs-user-config-file "/Users/d12frosted/.spacemacs_cfg.el")
+(d12-spacemacs-load
+ d12-path-spacemacs-distr-home
+ d12-path-spacemacs-distr-init-file
+ d12-path-spacemacs-user-config-file)
+
+;; The worst key binding ever! If I ever want to quit Emacs, I'd call my doctor.
+(unbind-key "C-x C-c")
+
+;; I use meta a lot, and command key is much easier to hit than option.
+(setq mac-command-modifier 'meta
+      mac-option-modifier  'none)
+
+;; (defun d12*kill-emacs (orig-fun &optional arg)
+;;   (when (y-or-n-p "Who dares to kill emacs? Are you sure?")
+;;     (apply orig-fun arg)))
+;; (advice-add 'kill-emacs :around #'d12*kill-emacs)
+
+;; (defun d12*message (orig-fun format-string &rest args)
+;;   (apply orig-fun (format "[%s] %s" (format-time-string "%H:%M:%S.%6N") format-string) args))
+;; (advice-add 'message :around #'d12*message)
+
+;;; init.el ends here
