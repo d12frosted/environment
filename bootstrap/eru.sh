e048c25eec1f47368cbce57bbbf52ae9e35deb8f
Boris Buliga <boris@d12frosted.io>
2018-02-05 22:49:10 +0200

    [bootstrap] little step

--- bootstrap/eru.sh
+++ bootstrap/eru.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+
+set -e
+
+target=$XDG_CONFIG_HOME
+if [[ "$target" = "" ]]; then
+  target="$HOME/.config"
+fi
+
+if [[ -d "$target" ]]; then
+  echo "$target already exists"
+else
+  git clone "git@github.com:d12frosted/environment.git" "$target"
+fi
+
+
+cd "$target" && {
+  bash "bootstrap/manwe.sh"
+}
+
+cd "$target/bootstrap/melkor" && {
+  stack clean
+  stack build --pedantic
+  stack exec melkor
+}
