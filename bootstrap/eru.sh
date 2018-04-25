c0022411cd6008da522a66cc4cbe1437dc39aa8e
Boris Buliga <boris@d12frosted.io>
2018-04-25 18:52:19 +0300

    [bootstrap] testing ci

--- bootstrap/eru.sh
+++ bootstrap/eru.sh
@@ -79,7 +79,7 @@ echo "set -U XDG_CONFIG_HOME ~/.config" | fish
 echo "set -x XDG_CONFIG_HOME ~/.config" | fish
 echo "set -x SPACEMACSDIR $XDG_CONFIG_HOME/emacs" | fish
 
-if [ "4" != $(emacs --batch --eval="(print (+ 2 2))") ]; then
+if [ "2" != $(emacs --batch --eval="(print (+ 2 2))") ]; then
   error "the world shall burn without Emacs on your side"
   exit 1
 fi
