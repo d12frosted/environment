925dc55f7c1b04867bff0efafc38a5b3e2a0b2de
Boris Buliga <boris@d12frosted.io>
2018-04-25 17:13:19 +0300

    [bootstrap] get rid of dotbot, sorry

--- bootstrap/eru.sh
+++ bootstrap/eru.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+function error() {
+  echo -e "\033[0;31m$@\033[0m"
+}
+
+if [[ "$(uname)" != "Darwin" ]]; then
+  error "Unsupported operating system: $(uname). This script works on Darwin only."
+  exit 1
+fi
+
 target=$XDG_CONFIG_HOME
 if [[ "$target" = "" ]]; then
   target="$HOME/.config"
@@ -18,13 +27,56 @@ else
   git clone "$repo" "$target"
 fi
 
-
-cd "$target" && {
-  bash "bootstrap/manwe.sh"
+function ensureDir() {
+  if [[ ! -d "$1" ]]; then
+    echo "Create $1"
+    mkdir -p "$1"
+  fi
 }
 
-# cd "$target/bootstrap/melkor" && {
-#   stack clean
-#   stack build --pedantic
-#   stack exec melkor
-# }
+ensureDir "$HOME/.local/bin"
+
+function check() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+check brew || {
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+}
+
+function safe_link() {
+  s="$target/$1"
+  t="${2/#\~/$HOME}"
+  d=$(dirname $t)
+  if [[ ! -f "$s" ]]; then
+    error "Can not link '$s' as it does not exist"
+    exit 1
+  fi
+
+  if [[ ! -d $d ]]; then
+    echo "create $d"
+    mkdir -p $d
+  fi
+
+  if [[ -L "$t" ]]; then
+    echo "relink $s -> $t"
+    rm "$t"
+  else
+    echo "link $s -> $t"
+  fi
+
+  ln -s "$s" "$t"
+}
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  safe_link $line
+done < "$target/bootstrap/Linkfile"
+
+cd "$target/bootstrap" && brew bundle
+
+echo "set -U XDG_CONFIG_HOME ~/.config" | fish
+echo "set -x XDG_CONFIG_HOME ~/.config" | fish
+echo "set -x SPACEMACSDIR $XDG_CONFIG_HOME/emacs" | fish
+
+test "4" = $(emacs --batch --eval="(print (+ 2 2))")
