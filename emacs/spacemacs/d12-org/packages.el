1c88f5f2d3a227d023c312905c4e092d1a137e70
Boris Buliga <boris@d12frosted.io>
2018-04-28 21:08:36 +0300

    [emacs] copyright year (again)

--- emacs/spacemacs/d12-org/packages.el
+++ emacs/spacemacs/d12-org/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- d12-org layer packages file for Spacemacs. -*- lexical-binding: t; -*-
 ;;
-;;; Copyright (c) 2015-2017 Boris Buliga
+;;; Copyright (c) 2015-2018 Boris Buliga
 ;;
 ;;; Author: Boris Buliga <boris@d12frosted.io>
 ;;
