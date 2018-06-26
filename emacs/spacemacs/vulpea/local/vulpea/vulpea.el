71db54d47f951ad94f7ddbd803b887a1b60887d1
Boris Buliga <boris@d12frosted.io>
2018-06-26 08:55:25 +0300

    [emacs] add properties formatting helpers to vulpea

--- emacs/spacemacs/vulpea/local/vulpea/vulpea.el
+++ emacs/spacemacs/vulpea/local/vulpea/vulpea.el
@@ -35,17 +35,12 @@
   (interactive)
   (insert (vulpea--make-brain-link (vulpea--choose-brain-entry))))
 
-(defvar vulpea-places-config
-  '(("country" . "EC4A5BD7-71C4-479A-8BBB-8F022E78F52D")))
-
-(defun vulpea/set-place ()
-  "Set place properties to org entry at point."
-  (interactive)
-  (seq-each
-   (lambda (cfg)
-     (unless (org-entry-get nil (car cfg))
-       (org-set-property (upcase (car cfg)) (vulpea--make-brain-link (vulpea--choose-brain-entry-by-parent (cdr cfg))))))
-   vulpea-places-config))
+(defun vulpea--make-brain-link (entry)
+  "Make an org-mode link to ENTRY."
+  (org-make-link-string
+   (concat "brain:"
+           (org-brain-entry-identifier entry))
+   (org-brain-title entry)))
 
 (defun vulpea--choose-brain-entry ()
   "Choose a brain entry."
@@ -63,12 +58,108 @@
    nil
    t))
 
-(defun vulpea--make-brain-link (entry)
-  "Make an org-mode link to ENTRY."
-  (org-make-link-string
-   (concat "brain:"
-           (org-brain-entry-identifier entry))
-   (org-brain-title entry)))
+
+
+(defvar vulpea-places-config
+  '(("country" . "EC4A5BD7-71C4-479A-8BBB-8F022E78F52D")))
+
+(defun vulpea/set-place ()
+  "Set place properties to org entry at point."
+  (interactive)
+  (seq-each
+   (lambda (cfg)
+     (unless (org-entry-get nil (car cfg))
+       (org-set-property (upcase (car cfg)) (vulpea--make-brain-link (vulpea--choose-brain-entry-by-parent (cdr cfg))))))
+   vulpea-places-config))
+
+
+
+(defvar vulpea-properties-order
+  '("ID" "CUSTOM_ID"
+    "TEA_GROUP"
+    "TAG"
+    "NAME" "NAME_ORIGINAL" "NAME_TRANSCRIPTION" "NAME_MEANING"
+    "YEAR_GATHERED" "YEAR_MANUFACTURED"
+    "PRESSING"
+    "COUNTRY" "PROVINCE" "PREFECTURE" "COUNTY" "TOWNSHIP" "VILLAGE" "MOUNTAIN" "LAKE" "PLACE"
+    "RATE"
+    "PRICE"
+    "AVAILABLE" "TOTAL_IN" "TOTAL_OUT"))
+
+(defun vulpea/sort-entry-properties ()
+  "Sort properties in entry at point."
+  (interactive)
+  (let ((p0 (car (org-get-property-block)))
+        (p1 (- (cdr (org-get-property-block)) 1))
+        (props (org-entry-properties))
+        (maxv (seq-length vulpea-properties-order))
+        (pregx "^:\\([a-zA-Z_\\-]+\\):.*$"))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region p0 p1)
+        (goto-char (point-min))
+        (let ;; To make `end-of-line' and etc. to ignore fields.
+            ((inhibit-field-text-motion t))
+          (sort-subr
+           nil 'forward-line 'end-of-line nil nil
+           (lambda (l1 l2)
+             (< (or
+                 (seq-position vulpea-properties-order
+                               (vulpea--match-regexp pregx l1))
+                 maxv)
+                (or
+                 (seq-position vulpea-properties-order
+                               (vulpea--match-regexp pregx l2))
+                 maxv)))))))))
+
+(defun vulpea/format-entry-properties ()
+  "Format properties in entry at point."
+  (interactive)
+  (let ((p0 (car (org-get-property-block)))
+        (p1 (cdr (org-get-property-block))))
+    (save-excursion
+      (goto-char p0)
+      (while (< (point) p1)
+        (org-indent-line)
+        (forward-line 1)
+        (beginning-of-line)))))
+
+(defun vulpea/pretty-entry-properties ()
+  "Prettify properties of entry at point.
+
+- `vulpea/format-entry-properties'
+- `vulpea/sort-entry-properties'"
+  (interactive)
+  (vulpea/format-entry-properties)
+  (vulpea/sort-entry-properties))
+
+(defun vulpea/pretty-buffer-properties ()
+  "Prettify properties of all entries in buffer.
+
+- `vulpea/format-entry-properties'
+- `vulpea/sort-entry-properties'"
+  (interactive)
+  (vulpea--map-outline
+   (lambda ()
+     (vulpea/format-entry-properties)
+     (vulpea/sort-entry-properties))))
+
+
+
+(defun vulpea--map-outline (f)
+  "Call function F on every outline in buffer."
+  (save-excursion
+    (beginning-of-buffer)
+    (while (search-forward-regexp outline-regexp)
+      (funcall f))))
+
+(defun vulpea--match-regexp (regexp val)
+  "Get the 1 match from the VAL."
+  (let ((s (if (stringp val)
+               val
+             (buffer-substring (car val) (cdr val)))))
+    (string-match regexp s)
+    (match-string 1 s)))
 
 (provide 'vulpea)
 
