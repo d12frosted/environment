f463e82906d0875dd2419391c7256e51a856c612
Boris Buliga <boris@d12frosted.io>
2018-07-11 12:17:11 +0300

    [emacs] setup eww package

--- emacs/spacemacs/d12-core/packages.el
+++ emacs/spacemacs/d12-core/packages.el
@@ -24,6 +24,7 @@
     evil-nerd-commenter
     ediff
     yasnippet
+    (eww :location built-in)
     ghub
     exec-path-from-shell
     )
@@ -93,6 +94,41 @@
     :config
     (mapc #'d12-path/make-directory-safe (yas-snippet-dirs))
     ))
+
+(defun d12-core/init-eww ()
+  ;; https://emacs.stackexchange.com/a/566/5161
+  (defvar-local d12-eww-display-images t)
+
+  (defun d12-eww/toggle-image-display ()
+    "Toggle images display on current buffer."
+    (interactive)
+    (setq d12-eww-display-images
+          (null d12-eww-display-images))
+    (d12-eww/backup-display-property d12-eww-display-images))
+
+  (defun d12-eww/backup-display-property (invert &optional object)
+    "Move the 'display property at POS to 'display-backup.
+Only applies if display property is an image.
+If INVERT is non-nil, move from 'display-backup to 'display
+instead.
+Optional OBJECT specifies the string or buffer. Nil means current
+buffer."
+    (let* ((inhibit-read-only t)
+           (from (if invert 'display-backup 'display))
+           (to (if invert 'display 'display-backup))
+           (pos (point-min))
+           left prop)
+      (while (and pos (/= pos (point-max)))
+        (if (get-text-property pos from object)
+            (setq left pos)
+          (setq left (next-single-property-change pos from object)))
+        (if (or (null left) (= left (point-max)))
+            (setq pos nil)
+          (setq prop (get-text-property left from object))
+          (setq pos (or (next-single-property-change left from object)
+                        (point-max)))
+          (when (eq (car prop) 'image)
+            (add-text-properties left pos (list from nil to prop) object)))))))
 
 (defun d12-core/init-ghub ()
   (use-package ghub
