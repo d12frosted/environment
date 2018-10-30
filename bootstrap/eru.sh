552c2d82d15cbde1b49b6c0b4c929a160f7b79cb
Boris Buliga <boris@d12frosted.io>
2018-10-30 20:53:04 +0200

    [bootstrap] add some simple logging

--- bootstrap/eru.sh
+++ bootstrap/eru.sh
@@ -34,10 +34,42 @@
 
 set -e
 
+#
+# Logging
+#
+
 function error() {
   echo -e "\033[0;31m$*\033[0m"
 }
 
+function intro() {
+  echo -e "\033[0;34m$*\033[0m"
+}
+
+function log() {
+  echo -e "\033[0;36m   $*\033[0m"
+}
+
+function theme() {
+  echo -e "\033[1;32m=> $1 Theme :: ${@:2}\033[0m"
+}
+
+#
+# Greetings
+#
+
+intro "Of the theme that I have declared to you, I will now that ye make in harmony
+together a Great Music. And since I have kindled you with the Flame
+Imperishable, ye shall show forth your powers in adorning this theme, each with
+his own thoughts and devices, if he will. But I win sit and hearken, and be glad
+that through you great beauty has been wakened into song."
+intro
+
+#
+# Operating system check
+#
+
+theme "Guardian" "Checking operating system"
 supported_os="Darwin"
 if [[ "$(uname)" != "$supported_os" ]]; then
   error "unsupported operating system: $(uname). This script works on $supported_os only."
@@ -48,9 +80,11 @@ fi
 # Helpers
 #
 
+theme "Supporting" "Defining helpers"
+
 function require_repo() {
   if [[ -d "$1/.git" ]]; then
-     echo "$1 already exists"
+    log "$1 already exists"
   else
     git clone "$2" "$1"
   fi
@@ -68,7 +102,7 @@ function require_github_repo() {
 }
 
 function sync_repo() {
-  echo "sync_repo $*"
+  log "sync_repo $*"
 
   wd=$(eval echo "$1")
   require_github_repo "$wd" "${@:2}"
@@ -123,7 +157,7 @@ function sync_repo() {
 
 function ensure_dir() {
   if [[ ! -d "$1" ]]; then
-    echo "create $1"
+    log "create $1"
     mkdir -p "$1"
   fi
 }
@@ -144,15 +178,15 @@ function safe_link() {
   fi
 
   if [[ ! -d $d ]]; then
-    echo "create $d"
+    log "create $d"
     mkdir -p "$d"
   fi
 
   if [[ -L "$t" ]]; then
-    echo "relink $s -> $t"
+    log "relink $s -> $t"
     rm "$t"
   else
-    echo "link $s -> $t"
+    log "link $s -> $t"
   fi
 
   ln -s "$s" "$t"
@@ -167,6 +201,8 @@ function map_lines() {
 #
 # Setup variables
 #
+
+theme "Supporting" "Defining variables"
 
 target=$XDG_CONFIG_HOME
 if [[ "$target" = "" ]]; then
@@ -242,13 +278,12 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 # Actual bootstrap
 #
 
-# install brew
-check brew || {
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew update
-}
+theme "Guardian" "Ensure all directories exists"
+ensure_dir "$HOME/.local/bin"
+ensure_dir "$DEVELOPER"
+ensure_dir "$HOME/Dropbox/Apps/Emacs"
 
-# setup SSH key
+theme "SSH" "Checking SSH keys"
 if [[ "$ALL" = "true" || "$SSH_KEY" = "true" ]]; then
   ssh_key_add_url="https://github.com/settings/ssh/new"
   ssh_key_path="$HOME/.ssh/id_rsa"
@@ -256,73 +291,75 @@ if [[ "$ALL" = "true" || "$SSH_KEY" = "true" ]]; then
   ssh_config_path="$HOME/.ssh/config"
 
   if [[ -f "$ssh_key_path" ]]; then
-    echo "SSH key found at $ssh_key_path."
+    log "SSH key found at $ssh_key_path."
   else
-    echo "No SSH key found."
+    log "No SSH key found."
     mkdir -p $(dirname "$ssh_key_path")
     ssh-keygen -t rsa -b 4096 -C "$USER" -f "$ssh_key_path"
-    echo "SSH key was generated."
+    log "SSH key was generated."
   fi
 
-  echo "Starting ssh-agent"
+  log "Starting ssh-agent"
   eval "$(ssh-agent -s)"
 
-  echo "Automatically load SSH key and use Keychain"
-  echo "Host *
+  log "Automatically load SSH key and use Keychain"
+  log "Host *
  AddKeysToAgent yes
  UseKeychain yes
  IdentityFile $ssh_key_path" > "$ssh_config_path"
 
-  echo "Add SSH key to ssh-agent"
+  log "Add SSH key to ssh-agent"
   ssh-add -K ~/.ssh/id_rsa
 
-  echo "Make sure to add SSH key to GitHub"
+  log "Make sure to add SSH key to GitHub"
   pbcopy < "$ssh_key_pub_path"
   open "$ssh_key_add_url"
   read -p "Press enter to continue"
 fi
 
-# clone dependencies
+theme "Repositories" "Sync repositories from Repofile"
 if [[ "$ALL" = "true" || "$REPO" = "true" ]]; then
   map_lines sync_repo "$target/bootstrap/Repofile"
 fi
 
-# create local directory for binaries
-ensure_dir "$HOME/.local/bin"
-ensure_dir "$DEVELOPER"
-ensure_dir "$HOME/Dropbox/Apps/Emacs"
-
-# run the Linkfile
+theme "Linking" "Link all files as defined in Linkfile"
 if [[ "$ALL" = "true" || "$LINK" = "true" ]]; then
   map_lines safe_link "$target/bootstrap/Linkfile"
 fi
 
-# run the Brewfile
+theme "Brew" "Ensure brew exists"
+check brew || {
+  info "Installing brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+}
+
+theme "Brew" "Install all dependencies"
 if [[ "$ALL" = "true" || "$BREW" = "true" ]]; then
   cd "$target/bootstrap" && brew bundle
 fi
 
-# setup fish shell
+theme "Fish" "Setup fish variables"
 echo "set -U XDG_CONFIG_HOME $target" | fish
 echo "set -x XDG_CONFIG_HOME $target" | fish
 echo "set -x SPACEMACSDIR $XDG_CONFIG_HOME/emacs" | fish
 
-# setup local git config
+theme "Git" "Create a local git config file"
 touch "$target/git/local.config"
 
-# write macOS defaults
+theme "macOS" "Write all defaults"
 if [[ "$ALL" = "true" || "$MACOS" = "true" ]]; then
   source "$target/macos/defaults.sh"
 fi
 
-# patch skhd
+theme "skhd" "Patch skhd application PATH"
 if [[ "$ALL" = "true" || "$SKHD" = "true" ]]; then
   check skhd && {
     "$target/utils/bin/patch_skhd_path"
   }
 fi
 
-# ensure that Emacs runs normally
+theme "Guarding" "Check that Emacs runs as expected"
 if [[ "$ALL" = "true" || "$TESTS" = "true" ]]; then
   emacs --batch -l "$target/emacs/test.el"
 fi
