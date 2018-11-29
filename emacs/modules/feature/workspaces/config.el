a0fc56bca70b3e16e131040e60d93cc52c131a5f
Boris Buliga <boris@d12frosted.io>
2018-11-29 11:36:02 +0200

    [emacs] remove redundant code

--- emacs/modules/feature/workspaces/config.el
+++ emacs/modules/feature/workspaces/config.el
@@ -127,8 +127,7 @@ Uses `+workspaces-main' to determine the name of the main workspace."
 
   ;; Delete the current workspace if closing the last open window
   (define-key! persp-mode-map
-    [remap delete-window] #'+workspace/close-window-or-workspace
-    [remap evil-delete-window] #'+workspace/close-window-or-workspace)
+    [remap delete-window] #'+workspace/close-window-or-workspace)
 
   ;; per-frame workspaces
   (setq persp-init-frame-behaviour t
