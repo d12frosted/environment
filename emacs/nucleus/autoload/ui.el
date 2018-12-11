0d8cf2a6fcbf0933ea86b070f13608b5fb6b1f17
Boris Buliga <boris@d12frosted.io>
2018-12-11 18:34:34 +0200

    [emacs] remove unused ui helper

--- emacs/nucleus/autoload/ui.el
+++ emacs/nucleus/autoload/ui.el
@@ -80,14 +80,3 @@ to undo."
                 (maximize-window))
             (maximize-window))
           t)))
-
-;;;###autoload
-(defun nucleus/set-frame-opacity (opacity)
-  "Interactively change the current frame's opacity.
-
-OPACITY is an integer between 0 to 100, inclusive."
-  (interactive
-   (list (read-number "Opacity (0-100): "
-                      (or (frame-parameter nil 'alpha)
-                          100))))
-  (set-frame-parameter nil 'alpha opacity))
