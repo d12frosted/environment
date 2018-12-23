9e6c8f93b41b0d143130493aa2ca203963716271
Boris Buliga <boris@d12frosted.io>
2018-12-23 16:35:55 +0000

    [fish] SSH agent

--- fish/config.fish
+++ fish/config.fish
@@ -72,6 +72,16 @@ if test -f $PRIVATE_FISH_CONFIGS_HOME/postconfig.fish
   source $PRIVATE_FISH_CONFIGS_HOME/postconfig.fish
 end
 
+# ssh-agent
+# https://github.com/danhper/fish-ssh-agent
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
+
 set fish_greeting "
              ;,'
      _o_    ;:;'
