5ea3096dae67959e17ddf5bbac563a0341a75d9c
Boris Buliga <boris@d12frosted.io>
2019-04-23 13:24:58 +0300

    [eru] debug lock service

--- eru.sh
+++ eru.sh
@@ -515,6 +515,7 @@ arch_guard && {
 
   theme_guard "hardware" "Setup autolock" && {
     sudo cp "$XDG_CONFIG_HOME/arch/lock@.service" /etc/systemd/system/lock@.service
+    ls -l /etc/systemd/system/sleep.target.wants
     systemctl enable "lock@${USER}.service"
   }
 }
