903b258c10c1f677fe053e5cb732d6d6a04179fd
Boris Buliga <boris@d12frosted.io>
2019-04-29 12:49:42 +0300

    [taffybar] better batter widget

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -4,22 +4,34 @@
 module Main (main) where
 
 --------------------------------------------------------------------------------
+
 import           Control.Applicative
+import           Control.Monad
 import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Reader
+import           Data.Int (Int64)
+import           Data.String
 import qualified Data.Text as T
 import           GI.Gtk ( Widget
-                        , eventBoxNew
                         , containerAdd
+                        , eventBoxNew
                         , eventBoxSetVisibleWindow
-                        , widgetShowAll
+                        , labelNew
+                        , labelSetMarkup
+                        , onWidgetRealize
                         , toWidget
+                        , widgetShowAll
                         )
 import           System.Taffybar
+import           System.Taffybar.Context (TaffyIO)
 import           System.Taffybar.Hooks
+import           System.Taffybar.Information.Battery
 import           System.Taffybar.SimpleConfig
 import           System.Taffybar.Util
 import           System.Taffybar.Widget
+import           System.Taffybar.Widget.Generic.ChannelWidget
 import           System.Taffybar.Widget.Generic.PollingLabel
+import           Text.Printf
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -38,7 +50,7 @@ config =
     , windowsNew defaultWindowsConfig ]
   , endWidgets = map (>>= buildContentsBox)
     [ textClockNew Nothing "%F (%a) %T" 0.5
-    , textBatteryNew "$percentage$% $time$ | "
+    , textBatteryNew' formatBatteryInfo
     , batteryIconNew
     , sniTrayThatStartsWatcherEvenThoughThisIsABadWayToDoIt
     , kbdNew 0.5
@@ -74,3 +86,51 @@ kbdNew updateSeconds = liftIO $ do
     handle :: Either String String -> T.Text
     handle (Left _) = "[??]"
     handle (Right r) = mconcat ["[", T.pack r, "]"]
+
+--------------------------------------------------------------------------------
+data BatteryWidgetInfo = BWI
+  { seconds :: Maybe Int64
+  , percent :: Int
+  , state :: BatteryState
+  } deriving (Eq, Show)
+
+textBatteryNew' :: (BatteryWidgetInfo -> T.Text) -> TaffyIO Widget
+textBatteryNew' format = do
+  chan <- getDisplayBatteryChan
+  ctx <- ask
+  let getLabelText info = format (getBatteryWidgetInfo info)
+      getBatteryInfoIO = runReaderT getDisplayBatteryInfo ctx
+  liftIO $ do
+    label' <- getLabelText <$> getBatteryInfoIO >>= labelNew . Just
+    let setMarkup text = postGUIASync $ labelSetMarkup label' text
+        updateWidget' = setMarkup . getLabelText
+    void $ onWidgetRealize label' $ getLabelText <$> getBatteryInfoIO >>= setMarkup
+    toWidget =<< channelWidgetNew label' chan updateWidget'
+
+getBatteryWidgetInfo :: BatteryInfo -> BatteryWidgetInfo
+getBatteryWidgetInfo info
+  = BWI
+  { seconds = battTime
+  , percent = battPctNum
+  , state = batteryState info
+  }
+  where
+    battPctNum :: Int
+    battPctNum = floor (batteryPercentage info)
+    battTime :: Maybe Int64
+    battTime = case batteryState info of
+      BatteryStateCharging -> Just $ batteryTimeToFull info
+      BatteryStateDischarging -> Just $ batteryTimeToEmpty info
+      _ -> Nothing
+
+formatBatteryInfo :: IsString a => BatteryWidgetInfo -> a
+formatBatteryInfo (BWI _ _ BatteryStateFullyCharged) = ""
+formatBatteryInfo (BWI Nothing p _) = fromString $ mconcat [show p, "%"]
+formatBatteryInfo (BWI s p _) = fromString $ mconcat [show p, "%", "(", formatDuration s, ")"]
+
+formatDuration :: Maybe Int64 -> String
+formatDuration Nothing = ""
+formatDuration (Just secs) = let minutes = secs `div` 60
+                                 hours = minutes `div` 60
+                                 minutes' = minutes `mod` 60
+                             in fromString $ printf "%02d:%02d" hours minutes'
