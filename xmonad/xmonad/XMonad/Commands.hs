0d55a624490fd58dafbb6ec3b6f6668c16d48abe
Boris Buliga <boris@d12frosted.io>
2019-05-14 20:23:16 +0300

    [xmonad] put all commands under commands

--- xmonad/xmonad/XMonad/Commands.hs
+++ xmonad/xmonad/XMonad/Commands.hs
@@ -9,6 +9,25 @@ import Control.Monad.IO.Class (MonadIO)
 import XMonad
 
 --------------------------------------------------------------------------------
+dmenu :: MonadIO m => m ()
+dmenu = spawn "dmenu_run -h 32 -fn 'Source Code Pro-14'"
+
+--------------------------------------------------------------------------------
+xlock :: MonadIO m => m ()
+xlock = spawn "xlocker"
+
+--------------------------------------------------------------------------------
+rebuild :: MonadIO m => m ()
+rebuild = spawn "eru xmonad"
+
+--------------------------------------------------------------------------------
+toggleKbd :: MonadIO m => m ()
+toggleKbd = spawn "switch_kbd_layout t"
+
+toggleKbd' :: MonadIO m => m ()
+toggleKbd' = spawn "switch_kbd_layout"
+
+--------------------------------------------------------------------------------
 vlmInc :: MonadIO m => m ()
 vlmInc = spawn "pactl set-sink-mute 0 false ; pactl set-sink-volume 0 +5%"
 
--- xmonad/xmonad/XMonad/Keybindings.hs
+++ xmonad/xmonad/XMonad/Keybindings.hs
@@ -13,14 +13,14 @@ import XMonad
 --------------------------------------------------------------------------------
 keybindings :: [((KeyMask, KeySym), X ())]
 keybindings =
-  [ ((mod4Mask, xK_q), spawn "eru xmonad")
-  , ((mod4Mask, xK_p), spawn "dmenu_run -h 32 -fn 'Source Code Pro-14'")
+  [ ((mod4Mask, xK_q), rebuild)
+  , ((mod4Mask, xK_p), dmenu)
   , ((0, xF86XK_AudioRaiseVolume), vlmInc)
   , ((0, xF86XK_AudioLowerVolume), vlmDec)
   , ((0, xF86XK_AudioMute), vlmMute)
   , ((0, xF86XK_AudioMicMute), micMute)
-  , ((mod4Mask .|. shiftMask, xK_z), spawn "xlocker")
-  , ((mod4Mask, xK_Escape), spawn "switch_kbd_layout t")
-  , ((mod4Mask .|. shiftMask, xK_Escape), spawn "switch_kbd_layout")
+  , ((mod4Mask .|. shiftMask, xK_z), xlock)
+  , ((mod4Mask, xK_Escape), toggleKbd)
+  , ((mod4Mask .|. shiftMask, xK_Escape), toggleKbd')
   , ((mod4Mask, xK_Print), spawn "scrot")
   ]
