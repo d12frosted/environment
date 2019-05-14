3c91ec2b112d6ab785f5e6efb8772d6ea02f291b
Boris Buliga <boris@d12frosted.io>
2019-05-14 19:53:20 +0300

    [xmonad] scrot only under mod4Mask

--- xmonad/src/xmonad.hs
+++ xmonad/src/xmonad.hs
@@ -78,7 +78,7 @@ extraKeys =
   , ((mod4Mask, xK_Escape), spawn "switch_kbd_layout t")
   , ((mod4Mask .|. shiftMask, xK_Escape), spawn "switch_kbd_layout")
   , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- one window
-  , ((0, xK_Print), spawn "scrot")
+  , ((mod4Mask, xK_Print), spawn "scrot")
   ]
 
 --------------------------------------------------------------------------------
