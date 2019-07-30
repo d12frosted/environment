1828319a3f0b3a3db4bd937657cb016932bc48fc
Boris Buliga <boris@d12frosted.io>
2019-07-30 21:09:38 +0300

    [emacs] quit dired-dired

---
+++ emacs/modules/emacs/dired/autoload.el
@@ -0,0 +1,24 @@
+;;; emacs/dired/autoload.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2019 Boris Buliga
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; Maintainer: Boris Buliga <boris@d12frosted.io>
+;;
+;; Created: 30 Jul 2019
+;;
+;; URL: https://github.com/d12frosted/environment/emacs
+;;
+;; License: GPLv3
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+;;;###autoload
+(defun +dired/quit-window ()
+  "Quit dired and kill buffers"
+  (interactive)
+  (quit-window t))
