786f3f3906a523924fab1914f485176de021bb04
Boris Buliga <boris@d12frosted.io>
2019-08-23 16:25:24 +0300

    [emacs] comments are comments

--- emacs/modules/config/os/config.el
+++ emacs/modules/config/os/config.el
@@ -17,19 +17,25 @@
 ;;
 ;;; Code:
 
+;;
+;; Environment modification
+
 (defmacro set-env! (&rest _vars)
   "Inject VARS from your shell environment into Emacs.")
+
+;;
+;; Mac related configurations
 
 (when IS-MAC
   (setq mac-command-modifier 'meta
         mac-option-modifier  'alt
         ;; sane trackpad/mouse scroll settings
         mac-redisplay-dont-reset-vscroll t
-	;; do not use smoth scrolling
+        ;; do not use smoth scrolling
         mac-mouse-wheel-smooth-scroll nil
-	;; scroll one line at a time
+        ;; scroll one line at a time
         mouse-wheel-scroll-amount '(1)
-	;; don't accelerate scrolling
+        ;; don't accelerate scrolling
         mouse-wheel-progressive-speed nil
         ;; Do not use native fullscreen mode
         ns-use-native-fullscreen nil
