26d653a1e2927efb3d89e39783624515d780173c
Boris Buliga <boris@d12frosted.io>
2019-11-02 10:10:33 +0200

    [emacs] add list extensions

---
+++ emacs/lisp/lib-list.el
@@ -0,0 +1,26 @@
+;;; lib-list.el --- list utilities -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2019 Boris Buliga
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; Maintainer: Boris Buliga <boris@d12frosted.io>
+;;
+;; Created: 01 Nov 2019
+;;
+;; URL:
+;;
+;; License: GPLv3
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(defun +enlist (exp)
+  "Return EXP wrapped in a list, or as-is if already a list."
+  (declare (pure t) (side-effect-free t))
+  (if (listp exp) exp (list exp)))
+
+(provide 'lib-list)
+;;; lib-list.el ends here
