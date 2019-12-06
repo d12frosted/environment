e46b13a842c60992a4d0af547bca89550bbfcca4
Boris Buliga <boris@d12frosted.io>
2019-12-06 17:39:15 +0200

    [emacs] ask for the volume when creating new wine entry

--- emacs/lisp/+org-wine.el
+++ emacs/lisp/+org-wine.el
@@ -223,6 +223,7 @@ option set in the options section.
       (+org-prompt-number-property "SUGAR")
       (+org-prompt-number-property "ALCOHOL")
       (+org-prompt-property "PRICE")
+      (org-set-property "VOLUME" nil)
       (+org-prompt-completing-property "COLOUR" wine-colours)
       (+org-prompt-completing-property "SWEETNESS" wine-sweetness-levels)
       (+org-prompt-completing-property "CARBONATION" wine-carbonation-types)
