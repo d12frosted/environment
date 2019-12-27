8183609325a115c88930efc76ff94758295a0a42
Boris Buliga <boris@d12frosted.io>
2019-12-27 09:07:57 +0200

    [emacs] do not configure codding system
    It should be configured outside of Emacs on the system level.

--- emacs/lisp/init-base.el
+++ emacs/lisp/init-base.el
@@ -26,22 +26,6 @@
 (setq user-full-name "Boris Buliga"
       user-mail-address "boris@d12frosted.io")
 
-;; UTF-8 as the default coding system
-(when (fboundp 'set-charset-priority)
-  (set-charset-priority 'unicode))
-(set-language-environment 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-selection-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
-(set-file-name-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8
-      default-process-coding-system '(utf-8 . utf-8))
-(setq-default buffer-file-coding-system 'utf-8)
-
 (setq-default
  ;; silence advised function warnings
  ad-redefinition-action 'accept
