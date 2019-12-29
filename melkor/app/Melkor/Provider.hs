dbe28a16e70b7f1f6c9352e3cd2463826a7bc5bc
Boris Buliga <boris@d12frosted.io>
2019-12-29 11:33:23 +0200

    [eru] extract Provider.Types module

--- melkor/app/Melkor/Provider.hs
+++ melkor/app/Melkor/Provider.hs
@@ -1,4 +1,4 @@
--- | This module defines all kind of providers.
+-- | Collection of available providers.
 
 --------------------------------------------------------------------------------
 
@@ -11,49 +11,22 @@
 
 --------------------------------------------------------------------------------
 
-module Melkor.Provider where
+module Melkor.Provider
+ ( repoProvider
+ , linkProvider
+
+ , module Melkor.Provider.Types
+ ) where
 
 --------------------------------------------------------------------------------
 
 import           Melkor.Dependency
+import           Melkor.Provider.Types
 
 --------------------------------------------------------------------------------
 
 import           RIO
 import           RIO.Directory
-
---------------------------------------------------------------------------------
-
-data Provider
-  = Provider
-  { providerSatisfies   :: Dependency -> Bool
-  , providerCheckStatus :: Dependency -> Eff DependencyStatus
-  , providerInstall     :: Dependency -> Eff ()
-  }
-
-data DependencyStatus
-  = DependencyMissing
-  | DependencyOutdated
-  | DependencyLatest
-  | DependencyUnknown
-
-instance Display DependencyStatus where
-  display DependencyMissing  = "missing"
-  display DependencyOutdated = "outdated"
-  display DependencyLatest   = "latest"
-  display DependencyUnknown  = "unknown"
-
---------------------------------------------------------------------------------
-
-class HasProviders env where
-  providersL :: Lens' env (TVar [Provider])
-
-addProvider :: ( MonadIO m, MonadReader r m, HasProviders r )
-            => Provider
-            -> m ()
-addProvider provider = do
-  providers <- view providersL
-  atomically $ modifyTVar providers (provider :)
 
 --------------------------------------------------------------------------------
 
@@ -65,7 +38,7 @@ repoProvider
       _        -> False
 
   , providerCheckStatus = \case
-      DRepo loc branch fp -> doesDirectoryExist fp >>= \case
+      DRepo _ _ fp -> doesDirectoryExist fp >>= \case
         True -> pure DependencyUnknown
         False -> pure DependencyMissing
       _ -> pure DependencyUnknown
@@ -82,40 +55,5 @@ linkProvider
   , providerCheckStatus = pure . const DependencyUnknown
   , providerInstall = pure . const ()
   }
-
---------------------------------------------------------------------------------
-
-newtype Ctx
-  = Ctx
-  { ctxLogFunc :: LogFunc
-  }
-
-instance HasLogFunc Ctx where
-  logFuncL = lens ctxLogFunc (\x y -> x { ctxLogFunc = y })
-
-newtype Eff a
-  = Eff
-  { unEff :: Ctx -> RIO Ctx a
-  } deriving (Functor)
-
-instance Applicative Eff where
-  pure a = Eff $ \ctx -> pure a
-  (Eff f) <*> (Eff v) = Eff $ \ctx -> f ctx <*> v ctx
-
-instance Monad Eff where
-  m >>= k = Eff $ \ctx -> do
-    a <- unEff m ctx
-    unEff (k a) ctx
-
-instance MonadIO Eff where
-  liftIO m = Eff $ \ctx -> liftIO m
-
-runEff :: ( HasLogFunc env ) => Eff a -> RIO env a
-runEff = runEff' (\env -> Ctx $ env ^. logFuncL)
-
-runEff' :: (env -> Ctx) -> Eff a -> RIO env a
-runEff' f (Eff m) = do
-  env <- asks f
-  runRIO env (m env)
 
 --------------------------------------------------------------------------------
---
+++ melkor/app/Melkor/Provider/Types.hs
@@ -0,0 +1,95 @@
+-- | Provider data type definition and everything you need to define a custom
+-- provider.
+
+--------------------------------------------------------------------------------
+
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+--------------------------------------------------------------------------------
+
+module Melkor.Provider.Types where
+
+--------------------------------------------------------------------------------
+
+import           Melkor.Dependency
+
+--------------------------------------------------------------------------------
+
+import           RIO
+
+--------------------------------------------------------------------------------
+
+data Provider
+  = Provider
+  { providerSatisfies   :: Dependency -> Bool
+  , providerCheckStatus :: Dependency -> Eff DependencyStatus
+  , providerInstall     :: Dependency -> Eff ()
+  }
+
+--------------------------------------------------------------------------------
+
+class HasProviders env where
+  providersL :: Lens' env (TVar [Provider])
+
+addProvider :: ( MonadIO m, MonadReader r m, HasProviders r )
+            => Provider
+            -> m ()
+addProvider provider = do
+  providers <- view providersL
+  atomically $ modifyTVar providers (provider :)
+
+--------------------------------------------------------------------------------
+
+data DependencyStatus
+  = DependencyMissing
+  | DependencyOutdated
+  | DependencyLatest
+  | DependencyUnknown
+
+instance Display DependencyStatus where
+  display DependencyMissing  = "missing"
+  display DependencyOutdated = "outdated"
+  display DependencyLatest   = "latest"
+  display DependencyUnknown  = "unknown"
+
+--------------------------------------------------------------------------------
+
+newtype Ctx
+  = Ctx
+  { ctxLogFunc :: LogFunc
+  }
+
+instance HasLogFunc Ctx where
+  logFuncL = lens ctxLogFunc (\x y -> x { ctxLogFunc = y })
+
+--------------------------------------------------------------------------------
+
+newtype Eff a
+  = Eff
+  { unEff :: Ctx -> RIO Ctx a
+  } deriving (Functor)
+
+instance Applicative Eff where
+  pure = Eff . const . pure
+  (Eff f) <*> (Eff v) = Eff $ \ctx -> f ctx <*> v ctx
+
+instance Monad Eff where
+  m >>= k = Eff $ \ctx -> do
+    a <- unEff m ctx
+    unEff (k a) ctx
+
+instance MonadIO Eff where
+  liftIO = Eff . const . liftIO
+
+runEff :: ( HasLogFunc env ) => Eff a -> RIO env a
+runEff = runEff' (\env -> Ctx $ env ^. logFuncL)
+
+runEff' :: (env -> Ctx) -> Eff a -> RIO env a
+runEff' f (Eff m) = do
+  env <- asks f
+  runRIO env (m env)
+
+
+--------------------------------------------------------------------------------
