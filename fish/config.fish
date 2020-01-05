0cab3e9d32aef73a71f9e4e1ad6d263b393d2dcb
Boris Buliga <boris@d12frosted.io>
2020-01-05 16:32:23 +0200

    [fish] install fisher to the data_home

--- fish/config.fish
+++ fish/config.fish
@@ -29,11 +29,11 @@ set -x XDG_DATA_HOME "$HOME/.local/share"
 
 # install fisher
 # TODO: move to Eru
-if not functions -q fisher
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+set -g fisher_path $XDG_DATA_HOME/fisher
+if not test -f $fisher_path/functions/fisher.fish
+  curl https://git.io/fisher --create-dirs -sLo $fisher_path/functions/fisher.fish
   fish -c fisher
 end
-set -g fisher_path $XDG_DATA_HOME/fisher
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 for file in $fisher_path/conf.d/*.fish
