99cadc9948e37e86bd55f58b8dc0523c06f43faf
Boris Buliga <boris@d12frosted.io>
2020-01-23 13:42:16 +0200

    [fish] setup projects homevariable

--- fish/config.fish
+++ fish/config.fish
@@ -26,6 +26,7 @@ set -l PRIVATE_FISH_CONFIGS_HOME $HOME/Dropbox/Apps/fish
 
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x PROJECTS_HOME "$HOME/Developer"
 
 # install fisher
 # TODO: move to Eru
