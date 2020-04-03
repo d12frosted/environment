cf65261d9886f9241c1d961a4bb613b12ddca971
Boris Buliga <boris@d12frosted.io>
2020-04-03 17:24:13 +0300

    [fish] put local/bin to the beginning of the path

--- fish/config.fish
+++ fish/config.fish
@@ -64,7 +64,6 @@ function __append_to_path -a path
     set -x PATH $path $PATH
   end
 end
-set -x PATH $HOME/.local/bin $PATH
 set -x PATH $XDG_CONFIG_HOME/bin $PATH
 __append_to_path $GEM_HOME/bin
 __append_to_path /usr/texbin
@@ -72,6 +71,7 @@ __append_to_path /usr/local/sbin
 __append_to_path /usr/local/opt/coreutils/libexec/gnubin
 __append_to_path /usr/local/opt/gnu-sed/libexec/gnubin
 __append_to_path $HOME/Dropbox/bin
+set -x PATH $HOME/.local/bin $PATH
 
 if command -v systemctl >/dev/null 2>&1
   systemctl --user import-environment PATH
