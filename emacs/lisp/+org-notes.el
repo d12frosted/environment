a1466835e0473e23c4653419232fc6cc215dab9a
Boris Buliga <boris@d12frosted.io>
2020-04-16 16:11:03 +0300

    [emacs] do not disable org-roam when minibuffer is on

--- emacs/lisp/+org-notes.el
+++ emacs/lisp/+org-notes.el
@@ -54,20 +54,21 @@
   "Setup current buffer for notes viewing and editing.
 
 If the current buffer is not a note, does nothing."
-  (if (and buffer-file-name
-           (string-equal +org-notes-directory
-                         (file-name-directory buffer-file-name)))
-      (progn
-        (unless (bound-and-true-p org-roam-mode)
-          (org-roam-mode 1))
-        (setq-local time-stamp-start "#\\+TIME-STAMP:[ 	]+\\\\?[\"<]+")
-        (setq org-roam-last-window (get-buffer-window))
-        (unless (eq 'visible (org-roam--current-visibility))
-          (delete-other-windows)
-          (call-interactively #'org-roam)))
-    (when (and (fboundp #'org-roam--current-visibility)
-               (eq 'visible (org-roam--current-visibility)))
-      (delete-window (get-buffer-window org-roam-buffer)))))
+  (unless (active-minibuffer-window)
+    (if (and buffer-file-name
+             (string-equal +org-notes-directory
+                           (file-name-directory buffer-file-name)))
+        (progn
+          (unless (bound-and-true-p org-roam-mode)
+            (org-roam-mode 1))
+          (setq-local time-stamp-start "#\\+TIME-STAMP:[ 	]+\\\\?[\"<]+")
+          (setq org-roam-last-window (get-buffer-window))
+          (unless (eq 'visible (org-roam--current-visibility))
+            (delete-other-windows)
+            (call-interactively #'org-roam)))
+      (when (and (fboundp #'org-roam--current-visibility)
+                 (eq 'visible (org-roam--current-visibility)))
+        (delete-window (get-buffer-window org-roam-buffer))))))
 
 (defun +org-notes-rebuild ()
   "Rebuild notes database."
