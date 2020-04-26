ffb53fab28cb4b375a5e48ff6784abd35fed183b
Boris Buliga <boris@d12frosted.io>
2020-04-26 13:44:37 +0300

    [melkor] run orphan and ambiguous checks

--- melkor/app/Main.hs
+++ melkor/app/Main.hs
@@ -1,14 +1,48 @@
 --------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+
+--------------------------------------------------------------------------------
 
 module Main where
 
 --------------------------------------------------------------------------------
 
-import           RIO
+import Melkor
+import Melkor.Types.Provider
+import Melkor.Types.Resource
+import RIO
+import qualified RIO.HashSet as HS
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = pure ()
+main = runMelkor melkor
+
+melkor :: RIO Melkor ()
+melkor = do
+  depend $ gitHubRepo "d12frosted" "hledger-imp"
+  depend $ gitHubRepo "d12frosted" "environment"
+  using dummyProvider
+  using smartProvider
 
 --------------------------------------------------------------------------------
+
+dummyProvider :: Provider
+dummyProvider =
+  mkProvider
+    "GitHub"
+    HS.empty
+    (const (pure True))
+    (const (pure Missing))
+    (const (pure ()))
+    (const (pure ()))
+
+smartProvider :: Provider
+smartProvider =
+  mkProvider
+    "GitLab"
+    HS.empty
+    (const (pure True))
+    (const (pure Missing))
+    (const (pure ()))
+    (const (pure ()))
