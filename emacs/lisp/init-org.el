190551654be8e807c0577ae0a3653872f280d43c
Boris Buliga <boris@d12frosted.io>
2020-05-25 12:51:45 +0300

    [emacs] monstrous +org-beginning-of-line

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -77,6 +77,8 @@ It is relative to `org-directory', unless it is absolute.")
   (require '+org-cigars)
   (require '+org-dependent)
 
+  (define-key org-mode-map (kbd "C-a") '+org-beginning-of-line)
+
   ;; open files in the same window
   (add-to-list 'org-link-frame-setup '(file . find-file))
   (setq org-indirect-buffer-display 'current-window)
@@ -549,6 +551,68 @@ used as title."
 ARGS are ignored."
   (let ((_ args)))
   (org-save-all-org-buffers))
+
+(defun +org-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line. If
+point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+When the point is on the heading, move to the beginning of title,
+but after any TODO keyword or priority cookie. Repeated usage
+moves to the beginning of the line.
+
+When the point is on the bullet, first move to the beginning of
+the item, but after bullet and check-box. Repeated usage
+moves to the first non-whitespace character and then to the
+beginning of the line.
+
+If ARG is not nil, move forward ARG lines first. If point reaches
+the beginning or end of the buffer, stop there."
+  (interactive "P")
+  (let ((origin (point)))
+    ;; move to the beginning of line, so regexp lookups properly capture context
+    (when (numberp arg)
+      (let ((line-move-visual nil))
+        (forward-line arg)))
+    (beginning-of-line)
+    (cond
+     ((let ((case-fold-search nil)) (looking-at org-complex-heading-regexp))
+      ;; at the headline, special position is before the title, but after any
+      ;; TODO keyword or priority cookie.
+      (let ((refpos (min (1+ (or (match-end 3) (match-end 2) (match-end 1)))
+			                   (line-end-position)))
+	          (bol (point)))
+        (when (or (> origin refpos) (= origin bol))
+	        (goto-char refpos))))
+
+     ((and (looking-at org-list-full-item-re)
+	         (memq (org-element-type (save-match-data (org-element-at-point)))
+		             '(item plain-list)))
+      ;; Set special position at first white space character after
+      ;; bullet, and check-box, if any.
+      (let ((after-bullet
+	           (let ((box (match-end 3)))
+	             (cond ((not box) (match-end 1))
+		                 ((eq (char-after box) ?\s) (1+ box))
+		                 (t box)))))
+        ;; since we want proper rotation, we go back to the original point so
+        ;; `bolp' works as expected
+        (goto-char origin)
+        (cond
+	       ((or (> origin after-bullet) (bolp))
+	        (goto-char after-bullet))
+         ((= origin after-bullet)
+          (back-to-indentation))
+         ((= origin (point))
+          (move-beginning-of-line 1)))))
+
+     (t
+      (back-to-indentation)
+      (when (= origin (point))
+        (move-beginning-of-line 1))))))
 
 (provide 'init-org)
 ;;; init-org.el ends here
