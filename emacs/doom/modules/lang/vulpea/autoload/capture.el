f947b3420cf2fe6f155d61e73abfb52b8a2be989
Boris Buliga <boris@d12frosted.io>
2020-06-04 11:37:41 +0300

    [emacs] remove unused code

--- emacs/doom/modules/lang/vulpea/autoload/capture.el
+++
@@ -1,7 +0,0 @@
-;;; lang/vulpea/autoload/capture.el -*- lexical-binding: t; -*-
-
-;;;###autoload
-(defun +capture-task ()
-  "A short-cut for capturing a todo task."
-  (interactive)
-  (org-capture nil "t"))
--- emacs/emacs-profiles.el
+++
@@ -1,3 +0,0 @@
-(("default"   . ((user-emacs-directory . "~/.config/emacs")))
- ("custom"    . ((user-emacs-directory . "~/.config/emacs")))
- ("doom"      . ((user-emacs-directory . "~/.doom-emacs"))))
