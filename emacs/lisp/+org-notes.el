7968fdcb69070b0035f8d39540791a0e32d97dce
Boris Buliga <boris@d12frosted.io>
2020-12-19 13:26:47 +0200

    [emacs] rich notes selector

--- emacs/lisp/+org-notes.el
+++ emacs/lisp/+org-notes.el
@@ -35,6 +35,7 @@
 (defvar org-attach-id-dir)
 (defvar org-link-bracket-re)
 (defvar org-any-link-re)
+(defvar org-roam-mode)
 (autoload 'seq-contains-p "seq")
 (autoload 'deft "deft")
 (autoload 'deft-refresh "deft")
@@ -90,6 +91,54 @@
   (interactive)
   (deft)
   (deft-refresh))
+
+(defun +org-notes-select (prompt &optional initial-prompt completions filter-fn)
+  "Select a note.
+
+PROMPT is a message to present.
+
+INITIAL-PROMPT is the initial title prompt.
+
+COMPLETIONS is a list of completions to be used instead of
+`org-roam--get-title-path-completions`.
+
+FILTER-FN is the name of a function to apply on the candidates
+which takes as its argument an alist of path-completions.  See
+`+org-notes--get-title-path-completions' for details."
+  (unless org-roam-mode (org-roam-mode))
+  (let* ((completions (or completions
+                          (+org-notes--get-title-path-completions)))
+         (completions (if filter-fn
+                          (seq-filter filter-fn completions)
+                        completions))
+         (title-with-tags (org-roam-completion--completing-read (concat prompt ": ") completions
+                                                                :initial-input initial-prompt))
+         (res (cdr (assoc title-with-tags completions)))
+         (id (+seq-singleton
+              (+seq-flatten
+               (+org-notes-get-file-id (plist-get res :path))))))
+    (plist-put res :id id)))
+
+(defun +org-notes--get-title-path-completions ()
+  "Return an alist for completion.
+
+The car is the displayed title for completion, and the cdr
+contains all the funny stuff."
+  (let* ((rows (org-roam-db-query [:select [files:file titles:title tags:tags files:meta] :from titles
+                                   :left :join tags
+                                   :on (= titles:file tags:file)
+                                   :left :join files
+                                   :on (= titles:file files:file)]))
+         completions)
+    (setq rows (seq-sort-by (lambda (x)
+                              (plist-get (nth 3 x) :mtime))
+                            #'time-less-p
+                            rows))
+    (dolist (row rows completions)
+      (pcase-let ((`(,file-path ,title ,tags) row))
+        (let ((k (org-roam--prepend-tag-string title tags))
+              (v (list :path file-path :title title :tags tags)))
+          (push (cons k v) completions))))))
 
 (defun +org-notes-find ()
   "Find a note."
