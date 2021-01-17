c4917d8f0a4ef2ca2e777c6834eae75e7bc44d8f
Boris Buliga <boris@d12frosted.io>
2021-01-17 16:53:37 +0200

    (emacs) do not try to setup notes in non org-mode buffer

--- emacs/lisp/+org-notes.el
+++ emacs/lisp/+org-notes.el
@@ -289,6 +289,7 @@ If the current buffer is not a note, does nothing."
 (defun +org-notes-buffer-p ()
   "Return non-nil if the currently visited buffer is a note."
   (and buffer-file-name
+       (eq major-mode 'org-mode)
        (string-prefix-p
         (expand-file-name (file-name-as-directory +org-notes-directory))
         (file-name-directory buffer-file-name))))
