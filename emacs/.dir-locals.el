5b5cd44e9b086ca73a12677c99c3ce475e255f8b
Boris Buliga <boris@d12frosted.io>
2021-02-11 11:29:18 +0200

    (emacs) treat warnings as errors during compilation

--- emacs/.dir-locals.el
+++ emacs/.dir-locals.el
@@ -1,4 +1,5 @@
-((emacs-lisp-mode . ((fill-column . 70)
+((emacs-lisp-mode . ((byte-compile-error-on-warn . t)
+                     (fill-column . 70)
                      (indent-tabs-mode . nil)
                      (elisp-lint-indent-specs . ((vulpea-utils-with-file . 1)
                                                  (vulpea-utils-with-note . 1)
