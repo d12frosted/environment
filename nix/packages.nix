da34cbef52c66ac9643f8937b3a52235b0f632fa
Boris Buliga <boris@d12frosted.io>
2021-09-17 17:09:24 +0300

    (nix) update list of packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -45,6 +45,7 @@ in [
   youtube-dl
   qutebrowser
 ] ++ lib.optionals stdenv.isLinux [
+  traceroute
   dropbox-cli
   emacsGit
   feh
@@ -57,7 +58,6 @@ in [
   tree
   xdg-utils
   zip
-  zoom-us
 ] ++ [
   # all things editor
   (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
@@ -89,4 +89,24 @@ in [
   source-sans-pro
   source-serif-pro
   symbola
+] ++ lib.optionals stdenv.isLinux [
+  # work
+  file
+  globalprotect-openconnect
+  jetbrains.idea-ultimate
+  networkmanager-openconnect
+  openconnect
+  patchelf
+  pkg-config
+  steam-run
+  zoom-us
+  (
+    let
+      python-packages-global = python-packages: with python-packages; [
+        pip
+        setuptools
+        importlib-resources
+      ];
+    in python39.withPackages python-packages-global
+  )
 ]
