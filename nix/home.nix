874f6e04b8515a2b396524a1e6d5094087ace3e3
Boris Buliga <boris@d12frosted.io>
2021-10-04 10:48:03 +0300

    (system) use xsession instead of xinit
    It's not clear from this commit why as the real reason is hidden in
    private scripts.
    
    In short, because of zoom-us that I have to use for work, I am forced
    to use desktop environment and display manager, otherwise zoom-us
    doesn't function properly - it either crashes or doesn't show any
    video.
    
    So my setup is:
    
    - os: Arch Linux
    - de: xfce with most of the stuff disabled
    - dm: LightDM
    
    Custom stuff is configured via xsession file

--- nix/home.nix
+++ nix/home.nix
@@ -38,18 +38,37 @@ in {
   };
 
   home.file.".xsession".text = ''
+    export PATH=/nix/var/nix/profiles/default/bin:$PATH
+    export PATH=/run/current-system/sw/bin:$PATH
+    export PATH=$HOME/.nix-profile/bin:$PATH
+    export PATH=/run/wrappers/bin:$PATH
     export PATH=$HOME/.local/bin:$PATH
     export PATH=$HOME/.config/bin:$PATH
+
     export XDG_CONFIG_HOME=${config.xdg.configHome}
+
+    userresources=$XDG_CONFIG_HOME/xorg/xresources
+    usermodmap=$XDG_CONFIG_HOME/xorg/xmodmap
+
+    if [ -f "$userresources" ]; then
+      xrdb -merge "$userresources"
+    fi
+
+    if [ -f "$usermodmap" ]; then
+      xmodmap "$usermodmap"
+    fi
 
     # I need this so much outside of Emacs.
     ${pkgs.xorg.xset}/bin/xset r rate 180 26
+
+    # Make sure that Caps doesn't miss its purpose
+    setxkbmap -option caps:ctrl_modifier
 
     # I rarely see my wallpapers. But when I do, I am happy.
     fehbg &
 
     # Because every X needs some algebra to tame it.
-    dbus-launch d12-xmonad
+    dbus-launch d12-xmonad --replace
   '';
   home.file.".xsession".executable = true;
 
